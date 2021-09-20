e89106093fb88b03d4d1cc05146be76c88a75843
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2021-09-20 09:00:08 +0200

    feat: added live Ethereum tests
    This required having an available Ethereum entry-point. To prevent
    leaking access details, these are accessed via environment variables.
    
    Tests therefore need to be run with these environment variables set in
    order not to fail those tests.
    
    During CI, these are set using github secrets.

--- src/ethereum.rs
+++ src/ethereum.rs
@@ -104,3 +104,78 @@ impl Client {
             .await
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::env;
+
+    use reqwest::Url;
+
+    use super::*;
+
+    /// Creates an [EthereumConfig] from environment variables.
+    ///
+    /// Done this way to prevent leaking URLs, passwords etc but we still
+    /// want to test this during CI.
+    fn eth_config_from_env() -> EthereumConfig {
+        let url = env::var("STARKNET_ETHEREUM_URL")
+            .expect("Ethereum URL environment var not set (STARKNET_ETHEREUM_URL)");
+        let user = env::var("STARKNET_ETHEREUM_USER").ok();
+        let password = env::var("STARKNET_ETHEREUM_PASSWORD").ok();
+
+        let url = url.parse::<Url>().expect("Bad ethereum URL");
+
+        EthereumConfig {
+            url,
+            user,
+            password,
+        }
+    }
+
+    /// Returns the known StarkNet state at a specific hash:
+    ///     (hash, root, sequence number)
+    fn known_state() -> (H256, U256, U256) {
+        let hash =
+            H256::from_str("4de373d45a29e0d6fe702f1d8c1d1bda81edc18a6409146af2dc6f9ea2f6503b")
+                .unwrap();
+
+        let root = U256::from_dec_str(
+            "1451723332915230892027004852411811409047732733786127578001959737407326381523",
+        )
+        .unwrap();
+
+        let sequence_number = U256::from_dec_str("19872").unwrap();
+
+        (hash, root, sequence_number)
+    }
+
+    #[tokio::test]
+    async fn latest_state_root() {
+        let client = Client::new(eth_config_from_env()).unwrap();
+        assert!(client.latest_state_root().await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn latest_state_sequence_number() {
+        let client = Client::new(eth_config_from_env()).unwrap();
+        assert!(client.latest_state_sequence_number().await.is_ok());
+    }
+
+    #[tokio::test]
+    async fn state_root_at_hash() {
+        let (hash, root, _) = known_state();
+        let client = Client::new(eth_config_from_env()).unwrap();
+        let result = client.state_root_at_hash(hash).await.unwrap();
+
+        assert_eq!(result, root);
+    }
+
+    #[tokio::test]
+    async fn state_sequence_number_at_hash() {
+        let (hash, _, sequence_number) = known_state();
+        let client = Client::new(eth_config_from_env()).unwrap();
+        let result = client.state_sequence_number_at_hash(hash).await.unwrap();
+
+        assert_eq!(result, sequence_number);
+    }
+}
