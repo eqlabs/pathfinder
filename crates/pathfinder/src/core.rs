681f470fd8554889cb478e4bc8b362171bcc7afb
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-01-21 15:39:54 +0200

    feat(core): add basic derives
    Copy, Clone, Debug, PartialEq

--- crates/pathfinder/src/core.rs
+++ crates/pathfinder/src/core.rs
@@ -5,26 +5,33 @@
 use pedersen::StarkHash;
 
 /// The address of a StarkNet contract.
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct ContractAddress(pub StarkHash);
 
 /// The hash of a StarkNet contract.
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct ContractHash(pub StarkHash);
 
 /// The hash of StarkNet contract's state. This is the value stored
 /// in the global state tree.
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct ContractStateHash(pub StarkHash);
 
 /// The commitment root of a StarkNet contract. This is the entry-point
 /// for a contract's state at a specific point in time via the contract
 /// state tree.
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct ContractRoot(pub StarkHash);
 
 /// The address of a storage element for a StarkNet contract.
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct StorageAddress(pub StarkHash);
 
 /// The value of a storage element for a StarkNet contract.
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct StorageValue(pub StarkHash);
 
 /// The commitment root of the global StarkNet state. This is the entry-point
 /// for the global state at a specific point in time via the global state tree.
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct GlobalRoot(pub StarkHash);
