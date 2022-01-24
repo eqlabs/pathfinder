c7664d87f2413ad2a6321ac207c02cfe44134403
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-01-24 21:32:57 +0200

    feat(core): state update now uses core types

--- crates/pathfinder/src/ethereum/state_update.rs
+++ crates/pathfinder/src/ethereum/state_update.rs
@@ -1,36 +1,40 @@
 mod parse;
 mod retrieve;
-mod state_root;
+pub mod state_root;
 
+use pedersen::StarkHash;
 use retrieve::*;
 
-use web3::{types::U256, Transport, Web3};
+use web3::{Transport, Web3};
 
-use crate::ethereum::{
-    log::StateUpdateLog,
-    state_update::{parse::StateUpdateParser, retrieve::retrieve_transition_fact},
+use crate::{
+    core::{ContractAddress, ContractHash, StorageAddress, StorageValue},
+    ethereum::{
+        log::StateUpdateLog,
+        state_update::{parse::StateUpdateParser, retrieve::retrieve_transition_fact},
+    },
 };
 
 /// Describes the deployment of a new StarkNet contract.
 #[derive(Debug, Clone, PartialEq)]
 pub struct DeployedContract {
-    pub address: U256,
-    pub hash: U256,
-    pub call_data: Vec<U256>,
+    pub address: ContractAddress,
+    pub hash: ContractHash,
+    pub call_data: Vec<StarkHash>,
 }
 
 /// A StarkNet contract's storage updates.
 #[derive(Debug, Clone, PartialEq)]
 pub struct ContractUpdate {
-    pub address: U256,
+    pub address: ContractAddress,
     pub storage_updates: Vec<StorageUpdate>,
 }
 
 /// A StarkNet contract's storage update.
 #[derive(Debug, Clone, PartialEq)]
 pub struct StorageUpdate {
-    pub address: U256,
-    pub value: U256,
+    pub address: StorageAddress,
+    pub value: StorageValue,
 }
 
 /// The set of state updates of a StarkNet [StateUpdate].
@@ -89,11 +93,18 @@ impl StateUpdate {
 mod tests {
     use std::str::FromStr;
 
+    use pedersen::StarkHash;
     use pretty_assertions::assert_eq;
     use web3::types::H256;
 
-    use crate::ethereum::{
-        test::create_test_websocket_transport, BlockOrigin, EthOrigin, TransactionOrigin,
+    use crate::{
+        core::{
+            EthereumBlockHash, EthereumBlockNumber, EthereumLogIndex, EthereumTransactionHash,
+            EthereumTransactionIndex, GlobalRoot, StarknetBlockNumber,
+        },
+        ethereum::{
+            test::create_test_websocket_transport, BlockOrigin, EthOrigin, TransactionOrigin,
+        },
     };
 
     use super::*;
@@ -103,26 +114,32 @@ mod tests {
         let update_log = StateUpdateLog {
             origin: EthOrigin {
                 block: BlockOrigin {
-                    hash: H256::from_str(
-                        "0x4de2eead55dfd20058fbbe77fb579cffb96985b26d86e24f1af361cf139b3a0d",
-                    )
-                    .unwrap(),
-                    number: 5973783,
+                    hash: EthereumBlockHash(
+                        H256::from_str(
+                            "0x4de2eead55dfd20058fbbe77fb579cffb96985b26d86e24f1af361cf139b3a0d",
+                        )
+                        .unwrap(),
+                    ),
+                    number: EthereumBlockNumber(5973783),
                 },
                 transaction: TransactionOrigin {
-                    hash: H256::from_str(
-                        "0xae7d1c87bc3a7ed34d129b5b037a0848f020bac1fa20082686399c88b538a6e2",
-                    )
-                    .unwrap(),
-                    index: 4,
+                    hash: EthereumTransactionHash(
+                        H256::from_str(
+                            "0xae7d1c87bc3a7ed34d129b5b037a0848f020bac1fa20082686399c88b538a6e2",
+                        )
+                        .unwrap(),
+                    ),
+                    index: EthereumTransactionIndex(4),
                 },
-                log_index: U256::from(23),
+                log_index: EthereumLogIndex(23),
             },
-            global_root: U256::from_dec_str(
-                "518441586592570845566219848761263100073689884954357429195191348826336924551",
-            )
-            .unwrap(),
-            block_number: U256::from(16407),
+            global_root: GlobalRoot(
+                StarkHash::from_hex_str(
+                    "1256D7337B57DD78AAA67563760FBDB561D7F51F335771E6D8D6CE60E4C1387",
+                )
+                .unwrap(),
+            ),
+            block_number: StarknetBlockNumber(16407),
         };
 
         let transport = create_test_websocket_transport().await;
@@ -130,109 +147,100 @@ mod tests {
 
         let expected = StateUpdate {
             deployed_contracts: vec![DeployedContract {
-                address: U256::from_dec_str(
-                    "192488165172431958337374653286540488197496085928528474156682062131548720794",
-                )
-                .unwrap(),
-                hash: U256::from_dec_str(
-                    "2044010574308692949556595542798601982234418418155425337239169936905147778109",
-                )
-                .unwrap(),
-                call_data: vec![U256::from_dec_str(
-                    "2642765183390518226191464338033793552381526284343500407573756018492213116591",
-                )
-                .unwrap()],
+                address: ContractAddress(StarkHash::from_hex_str("6CF1C6DCA6DE4CE15DB3EB7AEE1C6191537C82E2F2DE22FE4426199EE50E9A").unwrap()),
+                hash: ContractHash(StarkHash::from_hex_str("484DE75F165C844F9D8C5B07A7D1A650A476815DC7A061126FD41BB998C043D").unwrap()),
+                call_data: vec![StarkHash::from_hex_str("5D7C088BB051DB0A4C5A9C435A2009E2A82EE896080DDF4E6E953770F313EAF").unwrap()],
             }],
             contract_updates: vec![
                 ContractUpdate {
-                address: U256::from_dec_str("192488165172431958337374653286540488197496085928528474156682062131548720794").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("6CF1C6DCA6DE4CE15DB3EB7AEE1C6191537C82E2F2DE22FE4426199EE50E9A").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("944407150971126289822123988889809430356945584469838657342381477018902384507").unwrap(),
-                        value: U256::from_dec_str("192488165172431958337374653286540488197496085928528474156682062131548720794").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("21683F821A0574472445355BE6D2B769119E8515F8376A1D7878523DFDECF7B").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("6CF1C6DCA6DE4CE15DB3EB7AEE1C6191537C82E2F2DE22FE4426199EE50E9A").unwrap()),
                     },
                     StorageUpdate {
-                        address: U256::from_dec_str("1672321442399497129215646424919402195095307045612040218489019266998007191460").unwrap(),
-                        value: U256::from_dec_str("2642765183390518226191464338033793552381526284343500407573756018492213116591").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("3B28019CCFDBD30FFC65951D94BB85C9E2B8434111A000B5AFD533CE65F57A4").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("5D7C088BB051DB0A4C5A9C435A2009E2A82EE896080DDF4E6E953770F313EAF").unwrap()),
                     },
                     StorageUpdate {
-                        address: U256::from_dec_str("1697461057326310581967816530165551571743938660869987744467005324703617544296").unwrap(),
-                        value: U256::from_dec_str("1").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("3C0BA99F1A18BCDC81FCBCB6B4F15A9A6725F937075AED6FAC107FFCB147068").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("1").unwrap()),
                     },
                 ],
             },
             ContractUpdate {
-                address: U256::from_dec_str("448295659999083968328940375285038656322099894268333081962140244605900303949").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("FDB9F231A6C257D492DB4D091703ABA277E97B583AB9E3115B5A571FC22E4D").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("602215580536707347725838306637776804408199085594156310665557293021069653054").unwrap(),
-                        value: U256::from_dec_str("2896263172456672743997340121599341750222389451987581831383456686827572015356").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("154D7895A89D2A9EA002F6455F0BE1F409302F4E3A53B06B86C8A83E12D343E").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("6673A13BB9C8D4FD9ADFDF4AA0478BAFB4F6EE191D7BF31E3085136B803B0FC").unwrap()),
                     },
                 ],
             },
             ContractUpdate {
-                address: U256::from_dec_str("1165061033308622722462994167118484809586375016882777118347772110833430878236").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("29366B381BA18C53E9DB8A4476E0599C71CB63F001950D094CE23EDCD2CD81C").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("1540662175873464101179255226853115482807345027999462461827073633037049718373").unwrap(),
-                        value: U256::from_dec_str("10000000000000000000").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("367FBE030560FA33F15D19C5A53436B3345771F3769607B168E9BAFB540E665").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("8AC7230489E80000").unwrap()),
                     },
                     StorageUpdate {
-                        address: U256::from_dec_str("1540662175873464101179255226853115482807345027999462461827073633037049718374").unwrap(),
-                        value: U256::from_dec_str("0").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("367FBE030560FA33F15D19C5A53436B3345771F3769607B168E9BAFB540E666").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("0").unwrap()),
                     },
                 ],
             },
             ContractUpdate {
-                address: U256::from_dec_str("1349964407441356736445072259996730168659555931570900222313776602622042541087").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("2FC0D82D539509C5642B64F59299B7E9FD23C114BD2640BDC979602667F8C1F").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("373330842113182176873498781695511789062490522341957425172839703021474711994").unwrap(),
-                        value: U256::from_dec_str("2021").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("D34C3A8EDE05D741C7C11C8A517FEB3FEFCC425ED633E4D93758446BA289BA").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("7E5").unwrap()),
                     },
                 ],
             },
             ContractUpdate {
-                address: U256::from_dec_str("2244586465834706804729538181057356971546152457586787167114978055045865765708").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("4F664133F8C8C9A34B7D0B85AC09571BC92FBDC23CD7F82B0E8CEA3E3837B4C").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("373330842113182176873498781695511789062490522341957425172839703021474711994").unwrap(),
-                        value: U256::from_dec_str("1991").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("D34C3A8EDE05D741C7C11C8A517FEB3FEFCC425ED633E4D93758446BA289BA").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("7C7").unwrap()),
                     },
                 ],
             },
             ContractUpdate {
-                address: U256::from_dec_str("2986834203059737059446342291161493286636848077470151713641910585406717858830").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("69A7CFDF88197230CA4CE9377E1D8AAE7AD5E36E25DD35C7F3C73DAAD16940E").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("814079005391940027390129862062157285361348684878695833898695909074510122245").unwrap(),
-                        value: U256::from_dec_str("2560748179529625008397514353415812350378239053783288928353583273304539450250").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("1CCC09C8A19948E048DE7ADD6929589945E25F22059C7345AAF7837188D8D05").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("5A95504FA8DFC374D4072459027F6A50EAC084BB9F233DAE048C50FB8EECB8A").unwrap()),
                     },
                     StorageUpdate {
-                        address: U256::from_dec_str("1410752890141599390055702225444248987277077018130707938554244692172889272177").unwrap(),
-                        value: U256::from_dec_str("0").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("31E7534F8DDB1628D6E07DB5C743E33403B9A0B57508A93F4C49582040A2F71").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("0").unwrap()),
                     },
                     StorageUpdate {
-                        address: U256::from_dec_str("1563672576422918850564506150092036819309968525068313502302455251173901598124").unwrap(),
-                        value: U256::from_dec_str("1").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("37501DF619C4FC4E96F6C0243F55E3ABE7D1ACA7DB9AF8F3740BA3696B3FDAC").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("1").unwrap()),
                     },
                 ],
             },
             ContractUpdate {
-                address: U256::from_dec_str("3179147613456994138674947757137017509117110013980711456682683972427617641454").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("7075572D159FA30E93C6A917F75B5D664A99A7CEC4AF40FA7E6EF8094B7A3EE").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("5").unwrap(),
-                        value: U256::from_dec_str("1110").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("5").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("456").unwrap()),
                     },
                 ],
             },
             ContractUpdate {
-                address: U256::from_dec_str("3507237088938936343013853947406435564768985008074076680138339097283612102690").unwrap(),
+                address: ContractAddress(StarkHash::from_hex_str("7C1069DD27607ABF370C745B9781183FDD7E8082AC39C4E57D858913EE7D022").unwrap()),
                 storage_updates: vec![
                     StorageUpdate {
-                        address: U256::from_dec_str("5").unwrap(),
-                        value: U256::from_dec_str("102").unwrap(),
+                        address: StorageAddress(StarkHash::from_hex_str("5").unwrap()),
+                        value: StorageValue(StarkHash::from_hex_str("66").unwrap()),
                     },
                 ],
             },
