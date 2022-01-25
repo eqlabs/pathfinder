b2eb8094e12083a9cb97eab70c5b6c31886d436b
Joonas Koivunen <joonas@equilibrium.co>
2022-01-25 14:02:27 +0200

    feat: compute_contract_hash
    this is based on the cairo-lang v0.7.0 implementation in python and
    cairo. it leans heavily on serde_json::Value for (de)serialization and
    thus is quite expensive. only as little as possible of the contents of
    contract definition are being understood to be able to compute the hash.
    
    ideas attempted before:
    
    1. using only serde_json::Value
    2. learning of the full structure
    
    first suffers from contract_definition.program.hints ordering
    differently -- those are ints in python, so not ordered by string
    ordering.
    
    second suffers from the inability to find all of the pieces from the
    python codebase, some top level values are `Any` which makes this
    impossible, even if parts seem to have quite uniform structure.
    
    includes tests for the used serde_json features.

---
+++ crates/pathfinder/examples/compute_contract_hash.rs
@@ -0,0 +1,14 @@
+use std::io::Read;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if std::env::args().count() != 1 {
+        println!("compute_contract_hash -- reads from stdin, outputs a contract hash");
+        println!("the input read from stdin is expected to be a contract definition, which is a json blob.");
+        std::process::exit(1);
+    }
+    let mut s = String::new();
+    std::io::stdin().read_to_string(&mut s).unwrap();
+    let s = s;
+    println!("{:x}", pathfinder_lib::state::compute_contract_hash(&s)?);
+    Ok(())
+}
