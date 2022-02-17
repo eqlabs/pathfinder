f3979120d82fe7c99cf7c42cee4933df1721f18a
Joonas Koivunen <joonas@equilibrium.co>
2022-02-17 18:47:45 +0200

    doc: use more backticks

--- crates/pathfinder/src/state/merkle_tree.rs
+++ crates/pathfinder/src/state/merkle_tree.rs
@@ -331,7 +331,7 @@ impl<'a> MerkleTree<'a> {
     /// Deletes a leaf node from the tree.
     ///
     /// This is not an external facing API; the functionality is instead accessed by calling
-    /// [`MerkleTree::set`] with value set to [StarkHash::ZERO].
+    /// [`MerkleTree::set`] with value set to [`StarkHash::ZERO`].
     fn delete_leaf(&mut self, key: StarkHash) -> anyhow::Result<()> {
         // Algorithm explanation:
         //
