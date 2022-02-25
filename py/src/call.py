bf424a6dcc97aa8dd53c66cc5fe112ff0d695805
Joonas Koivunen <joonas@equilibrium.co>
2022-02-25 15:47:55 +0200

    doc(py): minor comment changes

--- py/src/call.py
+++ py/src/call.py
@@ -211,9 +211,9 @@ def check_schema(connection):
 def resolve_block(connection, at_block):
     from starkware.starknet.business_logic.state import BlockInfo
 
-    # FIXME: at least latest is wrong, and will be more wrong in the upcoming changes to schema
-
     if at_block == "latest":
+        # latest is questionable, but the rust side cannot use it at the moment at least,
+        # should probably be removed.
         cursor = connection.execute(
             "select number, timestamp, root from starknet_blocks order by number desc limit 1"
         )
@@ -235,6 +235,8 @@ def resolve_block(connection, at_block):
 
     try:
         [(block_number, block_time, global_root)] = cursor
+        # NOTE: this assumes the rust side to serialize starknet_blocks::timestamp as compatible
+        # for blocks before 0.7.0
         return (
             BlockInfo(block_number, block_time),
             global_root,
@@ -360,7 +362,7 @@ class SqliteAdapter:
         only = decompressor.decompress(only)
 
         # cairo-lang expects a ContractDefinitionFact, however we store just
-        # the contract definition over at pathfinder (from get_full_contract)
+        # the contract definition over at pathfinder (from full_contract)
         # so we need to wrap it up here. itertools is suggested by the manuals,
         # so lets hope it's the most efficient thing.
         #
