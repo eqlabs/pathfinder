1795df84048129d0d5f4e761a14ae78b237de432
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-03-18 22:39:20 +0200

    fix(logs): genesis query block range too long
    This patches an issue where some L1 nodes (geth), perform poorly for
    large search ranges. We have large search ranges while traversing the
    initial void where no Starknet exists on L1.
    
    We circumvent this by starting from the first L1 block containing
    Starknet, instead of starting from L1 genesis.

--- crates/pathfinder/src/ethereum/state_update/state_root.rs
+++ crates/pathfinder/src/ethereum/state_update/state_root.rs
@@ -1,14 +1,27 @@
-use crate::ethereum::{
-    log::{LogFetcher, StateUpdateLog},
-    Chain,
+use crate::{
+    core::EthereumBlockNumber,
+    ethereum::{
+        log::{LogFetcher, StateUpdateLog},
+        Chain,
+    },
 };
 
 /// A simple wrapper for [LogFetcher]<[StateUpdateLog]>.
 pub struct StateRootFetcher(LogFetcher<StateUpdateLog>);
 
+/// The Mainnet Ethereum block containing the Starknet genesis [StateUpdateLog].
+const MAINNET_GENESIS: EthereumBlockNumber = EthereumBlockNumber(13_627_224);
+/// The Goerli Ethereum block containing the Starknet genesis [StateUpdateLog].
+const GOERLI_GENESIS: EthereumBlockNumber = EthereumBlockNumber(5_854_324);
+
 impl StateRootFetcher {
     pub fn new(head: Option<StateUpdateLog>, chain: Chain) -> Self {
-        let inner = LogFetcher::<StateUpdateLog>::new(head, chain);
+        let genesis = match chain {
+            Chain::Mainnet => MAINNET_GENESIS,
+            Chain::Goerli => GOERLI_GENESIS,
+        };
+
+        let inner = LogFetcher::<StateUpdateLog>::new(head, chain, genesis);
         Self(inner)
     }
 }
