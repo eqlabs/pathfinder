b6e1944245301ead7af7afdfc2f7aa2b83674fa9
Joonas Koivunen <joonas@equilibrium.co>
2022-05-05 18:03:10 +0300

    refactor: less variables in user-agent

--- crates/pathfinder/src/consts.rs
+++ crates/pathfinder/src/consts.rs
@@ -1,10 +1,4 @@
 //! Repeated constants used around pathfinder
 
 /// User agent used in http clients
-///
-/// Resolves to "pathfinder/<version_info>"
-pub const USER_AGENT: &str = concat!(
-    env!("CARGO_PKG_NAME"),
-    "/",
-    env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT")
-);
+pub const USER_AGENT: &str = concat!("pathfinder/", env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT"));
