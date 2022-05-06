84e59c097b4cec60e4a354d0504de825c394026c
Krisztian Kovacs <krisztian@equilibrium.co>
2022-05-06 13:55:06 +0200

    fix(storage): add missing index on block hash for transactions
    According to load tests this speeds up starknet_getBlock* operations
    significantly.

--- crates/pathfinder/src/storage/schema.rs
+++ crates/pathfinder/src/storage/schema.rs
@@ -5,6 +5,7 @@ pub(crate) mod revision_0004;
 pub(crate) mod revision_0005;
 pub(crate) mod revision_0006;
 pub(crate) mod revision_0007;
+pub(crate) mod revision_0008;
 
 /// Used to indicate which action the caller should perform after a schema migration.
 #[derive(Debug, Clone, Copy, PartialEq)]
