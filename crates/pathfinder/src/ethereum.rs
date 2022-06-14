0b1bfc5775dac3f772475097b817881d44f408fe
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-06-14 16:31:21 +0200

    feat: impl Display for ethereum::Chain

--- crates/pathfinder/src/ethereum.rs
+++ crates/pathfinder/src/ethereum.rs
@@ -21,6 +21,15 @@ pub enum Chain {
     Goerli,
 }
 
+impl std::fmt::Display for Chain {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Chain::Mainnet => f.write_str("Mainnet"),
+            Chain::Goerli => f.write_str("Görli"),
+        }
+    }
+}
+
 /// List of semi-official Ethereum RPC errors taken from [EIP-1474] (which is stagnant).
 ///
 /// The issue of standardizing the Ethereum RPC seems to now be taking
