85bb337b274d1ee4675ffa20df8d43b415042519
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-07-18 12:18:31 +0200

    feat: tests for poll pending
    State diff test still fails as this logic is not correct.

--- crates/pathfinder/src/state/sync/pending.rs
+++ crates/pathfinder/src/state/sync/pending.rs
@@ -55,3 +55,201 @@ pub async fn poll_pending(
         tokio::time::sleep(poll_interval).await;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::poll_pending;
+    use crate::{
+        core::{
+            GasPrice, GlobalRoot, SequencerAddress, StarknetBlockHash, StarknetBlockNumber,
+            StarknetBlockTimestamp,
+        },
+        sequencer,
+    };
+
+    use assert_matches::assert_matches;
+    use stark_hash::StarkHash;
+
+    lazy_static::lazy_static!(
+        pub static ref PARENT_HASH: StarknetBlockHash =  StarknetBlockHash::from_hex_str("1234").unwrap();
+        pub static ref PARENT_ROOT: GlobalRoot = GlobalRoot(StarkHash::from_be_slice(b"parent root").unwrap());
+
+        pub static ref NEXT_BLOCK: sequencer::reply::Block = sequencer::reply::Block{
+            block_hash: StarknetBlockHash::from_hex_str("0xabcd").unwrap(),
+            block_number: StarknetBlockNumber(1),
+            gas_price: None,
+            parent_block_hash: *PARENT_HASH,
+            sequencer_address: None,
+            state_root: *PARENT_ROOT,
+            status: sequencer::reply::Status::AcceptedOnL2,
+            timestamp: StarknetBlockTimestamp(10),
+            transaction_receipts: Vec::new(),
+            transactions: Vec::new(),
+            starknet_version: None,
+        };
+
+        pub static ref PENDING_DIFF: sequencer::reply::StateUpdate = sequencer::reply::StateUpdate {
+            block_hash: None,
+            new_root: GlobalRoot(StarkHash::from_be_slice(b"new root").unwrap()),
+            old_root: *PARENT_ROOT,
+            state_diff: sequencer::reply::state_update::StateDiff {
+                storage_diffs: std::collections::HashMap::new(),
+                deployed_contracts: Vec::new(),
+                declared_contracts: Vec::new(),
+            }
+        };
+
+        pub static ref PENDING_BLOCK: sequencer::reply::PendingBlock = sequencer::reply::PendingBlock {
+            gas_price: GasPrice(11),
+            parent_hash: NEXT_BLOCK.parent_block_hash,
+            sequencer_address: SequencerAddress(StarkHash::from_be_slice(b"seqeunecer address").unwrap()),
+            status: sequencer::reply::Status::Pending,
+            timestamp: StarknetBlockTimestamp(20),
+            transaction_receipts: Vec::new(),
+            transactions: Vec::new(),
+            starknet_version: None,
+        };
+    );
+
+    /// Arbitrary timeout for receiving emits on the tokio channel. Otherwise failing tests will
+    /// need to timeout naturally which may be forever.
+    const TEST_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(5);
+
+    #[tokio::test]
+    async fn exits_on_full_block() {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let mut sequencer = sequencer::MockClientApi::new();
+
+        // Give a pending state update and full block.
+        sequencer.expect_block().returning(move |_| {
+            Ok(sequencer::reply::MaybePendingBlock::Block(
+                NEXT_BLOCK.clone(),
+            ))
+        });
+        sequencer
+            .expect_state_update()
+            .returning(move |_| Ok(PENDING_DIFF.clone()));
+
+        let jh = tokio::spawn(async move {
+            poll_pending(tx, &sequencer, *PARENT_HASH, std::time::Duration::ZERO).await
+        });
+
+        let result = tokio::time::timeout(TEST_TIMEOUT, rx.recv())
+            .await
+            .expect("Channel should be dropped");
+        assert_matches!(result, None);
+        jh.await.unwrap().unwrap();
+    }
+
+    #[tokio::test]
+    async fn exits_on_full_state_diff() {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let mut sequencer = sequencer::MockClientApi::new();
+
+        // A full diff has the block hash set.
+        let mut full_diff = PENDING_DIFF.clone();
+        full_diff.block_hash = Some(NEXT_BLOCK.block_hash);
+
+        sequencer.expect_block().returning(move |_| {
+            Ok(sequencer::reply::MaybePendingBlock::Pending(
+                PENDING_BLOCK.clone(),
+            ))
+        });
+        sequencer
+            .expect_state_update()
+            .returning(move |_| Ok(full_diff.clone()));
+
+        let jh = tokio::spawn(async move {
+            poll_pending(tx, &sequencer, *PARENT_HASH, std::time::Duration::ZERO).await
+        });
+
+        let result = tokio::time::timeout(TEST_TIMEOUT, rx.recv())
+            .await
+            .expect("Channel should be dropped");
+        assert_matches!(result, None);
+        jh.await.unwrap().unwrap();
+    }
+
+    #[tokio::test]
+    async fn exits_on_block_discontinuity() {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let mut sequencer = sequencer::MockClientApi::new();
+
+        let mut pending_block = PENDING_BLOCK.clone();
+        pending_block.parent_hash = StarknetBlockHash::from_hex_str("0xFFFFFF").unwrap();
+        sequencer.expect_block().returning(move |_| {
+            Ok(sequencer::reply::MaybePendingBlock::Pending(
+                pending_block.clone(),
+            ))
+        });
+        sequencer
+            .expect_state_update()
+            .returning(move |_| Ok(PENDING_DIFF.clone()));
+
+        let jh = tokio::spawn(async move {
+            poll_pending(tx, &sequencer, *PARENT_HASH, std::time::Duration::ZERO).await
+        });
+
+        let result = tokio::time::timeout(TEST_TIMEOUT, rx.recv())
+            .await
+            .expect("Channel should be dropped");
+        assert_matches!(result, None);
+        jh.await.unwrap().unwrap();
+    }
+
+    #[tokio::test]
+    async fn exits_on_state_diff_discontinuity() {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let mut sequencer = sequencer::MockClientApi::new();
+
+        sequencer.expect_block().returning(move |_| {
+            Ok(sequencer::reply::MaybePendingBlock::Pending(
+                PENDING_BLOCK.clone(),
+            ))
+        });
+
+        let mut disconnected_diff = PENDING_DIFF.clone();
+        disconnected_diff.old_root =
+            GlobalRoot(StarkHash::from_be_slice(b"different old root").unwrap());
+        sequencer
+            .expect_state_update()
+            .returning(move |_| Ok(disconnected_diff.clone()));
+
+        let jh = tokio::spawn(async move {
+            poll_pending(tx, &sequencer, *PARENT_HASH, std::time::Duration::ZERO).await
+        });
+
+        let result = tokio::time::timeout(TEST_TIMEOUT, rx.recv())
+            .await
+            .expect("Channel should be dropped");
+        assert_matches!(result, None);
+        jh.await.unwrap().unwrap();
+    }
+
+    #[tokio::test]
+    async fn success() {
+        let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+        let mut sequencer = sequencer::MockClientApi::new();
+
+        sequencer.expect_block().returning(move |_| {
+            Ok(sequencer::reply::MaybePendingBlock::Pending(
+                PENDING_BLOCK.clone(),
+            ))
+        });
+        sequencer
+            .expect_state_update()
+            .returning(move |_| Ok(PENDING_DIFF.clone()));
+
+        let _jh = tokio::spawn(async move {
+            poll_pending(tx, &sequencer, *PARENT_HASH, std::time::Duration::ZERO).await
+        });
+
+        let result = tokio::time::timeout(TEST_TIMEOUT, rx.recv())
+            .await
+            .expect("Event should be emitted")
+            .unwrap();
+
+        use crate::state::l2::Event::Pending;
+        assert_matches!(result, Pending(block, diff) if *block == *PENDING_BLOCK && *diff == *PENDING_DIFF);
+    }
+}
