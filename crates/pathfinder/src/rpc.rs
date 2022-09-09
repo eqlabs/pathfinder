543ca9eeb7d7cc138086e6c31045a82ed8232988
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-09-09 12:24:51 +0200

    feat: without async trait

--- crates/pathfinder/src/rpc.rs
+++ crates/pathfinder/src/rpc.rs
@@ -359,8 +359,49 @@ Hint: If you are looking to run two instances of pathfinder, you must configure 
             },
         )?;
 
-        let module = module.into_inner();
+        let mut module = module.into_inner();
+        Self::register_method2(
+            &mut module,
+            "starknet_getNonce",
+            get_nonce::GetNonce::execute,
+        )?;
+
         Ok(server.start(module).map(|handle| (handle, local_addr))?)
+    }
+
+    fn register_method2<Input, Output, Error, MethodFuture, Method>(
+        module: &mut jsonrpsee::RpcModule<RpcApi>,
+        method_name: &'static str,
+        method: Method,
+    ) -> anyhow::Result<()>
+    where
+        Input: ::serde::de::DeserializeOwned + Send + Sync,
+        Output: 'static + ::serde::Serialize + Send + Sync,
+        Error: Into<error::RpcError>,
+        MethodFuture: std::future::Future<Output = Result<Output, Error>> + Send,
+        Method: (Fn(std::sync::Arc<RpcApi>, Input) -> MethodFuture) + Copy + Send + Sync + 'static,
+    {
+        use tracing::Instrument;
+
+        metrics::register_counter!("rpc_method_calls_total", "method" => method_name);
+
+        let method_callback = move |params: jsonrpsee::types::Params<'static>, context| {
+            let span = tracing::info_span!("rpc_method", name = method_name);
+            async move {
+                let input = params.parse::<Input>()?;
+                method(context, input).await.map_err(|err| {
+                    let rpc_err: error::RpcError = err.into();
+                    jsonrpsee::core::Error::from(rpc_err)
+                })
+            }
+            .instrument(span)
+        };
+
+        module
+            .register_async_method(method_name, method_callback)
+            .with_context(|| format!("Registering {method_name}"))?;
+
+        Ok(())
     }
 
     fn register_method<T: RpcMethod>(
