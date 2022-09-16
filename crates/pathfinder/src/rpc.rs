ed55b3e7461cdf2ccd144c84655ae1dd599d7939
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-09-16 09:58:34 +0200

    refactor: move rpc wrapper into version one module

--- crates/pathfinder/src/rpc.rs
+++ crates/pathfinder/src/rpc.rs
@@ -24,50 +24,6 @@ use std::{net::SocketAddr, result::Result};
 use v01::api::{BlockResponseScope, RpcApi};
 use v01::types::request::{Call, ContractCall, EventFilter};
 
-/// Helper wrapper for attaching spans to rpc method implementations
-struct RpcModuleWrapper<Context>(jsonrpsee::RpcModule<Context>);
-
-impl<Context: Send + Sync + 'static> RpcModuleWrapper<Context> {
-    /// This wrapper helper adds a tracing span around all rpc methods with name = method_name.
-    ///
-    /// It could do more, for example trace the outputs, durations.
-    ///
-    /// This is the only one method provided at the moment, because it's the only one used. If you
-    /// need to use some other `register_*` method from [`jsonrpsee::RpcModule`], just add it to
-    /// this wrapper.
-    fn register_async_method<R, Fun, Fut>(
-        &mut self,
-        method_name: &'static str,
-        callback: Fun,
-    ) -> Result<
-        jsonrpsee::core::server::rpc_module::MethodResourcesBuilder<'_>,
-        jsonrpsee::core::Error,
-    >
-    where
-        R: ::serde::Serialize + Send + Sync + 'static,
-        Fut: std::future::Future<Output = Result<R, jsonrpsee::core::Error>> + Send,
-        Fun: (Fn(jsonrpsee::types::Params<'static>, std::sync::Arc<Context>) -> Fut)
-            + Copy
-            + Send
-            + Sync
-            + 'static,
-    {
-        use tracing::Instrument;
-
-        metrics::register_counter!("rpc_method_calls_total", "method" => method_name);
-
-        self.0.register_async_method(method_name, move |p, c| {
-            // why info here? it's the same used in warp tracing filter for example.
-            let span = tracing::info_span!("rpc_method", name = method_name);
-            callback(p, c).instrument(span)
-        })
-    }
-
-    fn into_inner(self) -> jsonrpsee::RpcModule<Context> {
-        self.0
-    }
-}
-
 pub struct RpcServer {
     addr: SocketAddr,
     api: RpcApi,
@@ -116,7 +72,7 @@ Hint: If you are looking to run two instances of pathfinder, you must configure 
                 _ => anyhow::Error::new(e),
             })?;
         let local_addr = server.local_addr()?;
-        let mut module = RpcModuleWrapper(RpcModule::new(self.api));
+        let mut module = v01::RpcModuleWrapper::new(RpcModule::new(self.api));
         module.register_async_method(
             "starknet_getBlockWithTxHashes",
             |params, context| async move {
--- crates/pathfinder/src/rpc/v01.rs
+++ crates/pathfinder/src/rpc/v01.rs
@@ -1,2 +1,50 @@
 pub mod api;
 pub mod types;
+
+/// Helper wrapper for attaching spans to rpc method implementations
+pub struct RpcModuleWrapper<Context>(jsonrpsee::RpcModule<Context>);
+
+impl<Context: Send + Sync + 'static> RpcModuleWrapper<Context> {
+    pub fn new(context: jsonrpsee::RpcModule<Context>) -> Self {
+        Self(context)
+    }
+
+    /// This wrapper helper adds a tracing span around all rpc methods with name = method_name.
+    ///
+    /// It could do more, for example trace the outputs, durations.
+    ///
+    /// This is the only one method provided at the moment, because it's the only one used. If you
+    /// need to use some other `register_*` method from [`jsonrpsee::RpcModule`], just add it to
+    /// this wrapper.
+    pub fn register_async_method<R, Fun, Fut>(
+        &mut self,
+        method_name: &'static str,
+        callback: Fun,
+    ) -> Result<
+        jsonrpsee::core::server::rpc_module::MethodResourcesBuilder<'_>,
+        jsonrpsee::core::Error,
+    >
+    where
+        R: ::serde::Serialize + Send + Sync + 'static,
+        Fut: std::future::Future<Output = Result<R, jsonrpsee::core::Error>> + Send,
+        Fun: (Fn(jsonrpsee::types::Params<'static>, std::sync::Arc<Context>) -> Fut)
+            + Copy
+            + Send
+            + Sync
+            + 'static,
+    {
+        use tracing::Instrument;
+
+        metrics::register_counter!("rpc_method_calls_total", "method" => method_name);
+
+        self.0.register_async_method(method_name, move |p, c| {
+            // why info here? it's the same used in warp tracing filter for example.
+            let span = tracing::info_span!("rpc_method", name = method_name);
+            callback(p, c).instrument(span)
+        })
+    }
+
+    pub fn into_inner(self) -> jsonrpsee::RpcModule<Context> {
+        self.0
+    }
+}
