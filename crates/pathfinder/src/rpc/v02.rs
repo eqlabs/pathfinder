f7d02056988e801fa2149c048aeb403350d5e2ba
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-09-23 14:43:15 +0200

    feat(rpc/v0.2): add sync status to context
    Also slight shuffling of items.

--- crates/pathfinder/src/rpc/v02.rs
+++ crates/pathfinder/src/rpc/v02.rs
@@ -1,25 +1,32 @@
+use std::sync::Arc;
+
+use crate::state::SyncState;
 use crate::{state::PendingData, storage::Storage};
 
 pub mod method;
 pub mod types;
 
+#[derive(Clone)]
 pub struct RpcContext {
     pub storage: Storage,
     pub pending_data: Option<PendingData>,
+    pub sync_status: Arc<SyncState>,
 }
 
 impl RpcContext {
-    pub fn new(storage: Storage) -> Self {
+    pub fn new(storage: Storage, sync_status: Arc<SyncState>) -> Self {
         Self {
             storage,
+            sync_status,
             pending_data: None,
         }
     }
 
     #[cfg(test)]
-    pub fn for_tests() -> std::sync::Arc<Self> {
+    pub fn for_tests() -> Arc<Self> {
         let storage = super::tests::setup_storage();
-        std::sync::Arc::new(Self::new(storage))
+        let sync_state = Arc::new(SyncState::default());
+        Arc::new(Self::new(storage, sync_state))
     }
 
     pub fn with_pending_data(self, pending_data: PendingData) -> Self {
@@ -30,9 +37,13 @@ impl RpcContext {
     }
 
     #[cfg(test)]
-    pub async fn for_tests_with_pending() -> std::sync::Arc<Self> {
-        let storage = super::tests::setup_storage();
-        let pending_data = super::tests::create_pending_data(storage.clone()).await;
-        std::sync::Arc::new(Self::new(storage).with_pending_data(pending_data))
+    pub async fn for_tests_with_pending() -> Arc<Self> {
+        // This is a bit silly with the arc in and out, but since its for tests the ergonomics of
+        // having Arc also constructed is nice.
+        let context = Self::for_tests();
+        let pending_data = super::tests::create_pending_data(context.storage.clone()).await;
+        let context = (*context).clone().with_pending_data(pending_data);
+
+        Arc::new(context)
     }
 }
