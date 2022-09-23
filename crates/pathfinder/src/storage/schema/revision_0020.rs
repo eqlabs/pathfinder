6eda060ffc1461d291f9fd652e2e420d29746d5d
Krzysztof Lis <klis33@gmail.com>
2022-09-23 11:03:35 +0200

    chore: fmt

--- crates/pathfinder/src/storage/schema/revision_0020.rs
+++ crates/pathfinder/src/storage/schema/revision_0020.rs
@@ -153,7 +153,9 @@ fn process_transaction(
     let contract_address = *transaction.contract_address().get();
 
     // remove transfer events
-    receipt.events.retain(|e| !is_transfer_event(contract_address, e));
+    receipt
+        .events
+        .retain(|e| !is_transfer_event(contract_address, e));
 
     if num_events != receipt.events.len() {
         update_database(tx, compressor, transaction_hash, block_number, receipt)?;
