fa486d551f4cd0ecd132d9e674e0542b30544a13
Krzysztof Lis <klis33@gmail.com>
2022-09-26 17:03:08 +0200

    feat: register chain_id

--- crates/pathfinder/src/rpc/v02.rs
+++ crates/pathfinder/src/rpc/v02.rs
@@ -150,6 +150,7 @@ where
 
 // Registers all methods for the v0.2 API
 pub fn register_all_methods(module: &mut jsonrpsee::RpcModule<RpcContext>) -> anyhow::Result<()> {
+    register_method_with_no_input(module, "starknet_chainId", method::chain_id::chain_id)?;
     register_method(module, "starknet_getNonce", method::get_nonce::get_nonce)?;
     register_method(
         module,
