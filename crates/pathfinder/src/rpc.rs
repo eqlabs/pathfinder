6127cd94eb0fdad16973874c3e431c9f9ffa6a64
Krzysztof Lis <klis33@gmail.com>
2022-09-27 09:33:29 +0200

    feat: use dotted versioning path

--- crates/pathfinder/src/rpc.rs
+++ crates/pathfinder/src/rpc.rs
@@ -78,8 +78,8 @@ Hint: If you are looking to run two instances of pathfinder, you must configure 
 
         Ok(server
             .start_with_paths([
-                (vec!["/", "/rpc/v01"], module_v01),
-                (vec!["/rpc/v02"], module_v02),
+                (vec!["/", "/rpc/v0.1"], module_v01),
+                (vec!["/rpc/v0.2"], module_v02),
             ])
             .map(|handle| (handle, local_addr))?)
     }
