cdff2994154155bf193c9f76d0ce35d134aeada9
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-11-14 12:47:41 +0200

    feat(rpc): simplify pathfinder_version
    Co-authored-by: Krzysztof Lis <chris@equilibrium.co>

--- crates/pathfinder/src/rpc/pathfinder.rs
+++ crates/pathfinder/src/rpc/pathfinder.rs
@@ -2,15 +2,10 @@ pub fn register_all_methods(module: &mut jsonrpsee::RpcModule<()>) -> anyhow::Re
     use anyhow::Context;
 
     module
-        .register_async_method(
-            "pathfinder_version",
-            |_, _| async move { Ok(version().await) },
-        )
+        .register_method("pathfinder_version", |_, _| {
+            Ok(env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT"))
+        })
         .with_context(|| format!("Registering pathfinder_version"))?;
 
     Ok(())
-}
-
-async fn version() -> &'static str {
-    return env!("VERGEN_GIT_SEMVER_LIGHTWEIGHT");
 }
