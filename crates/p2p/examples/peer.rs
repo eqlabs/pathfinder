b59482622035860967a218f530f31133c9eea6dc
Krisztian Kovacs <krisztian@equilibrium.co>
2022-11-16 11:32:21 +0100

    fix(p2p/peer): show target in tracing output

--- crates/p2p/examples/peer.rs
+++ crates/p2p/examples/peer.rs
@@ -174,7 +174,6 @@ async fn main() -> anyhow::Result<()> {
 fn setup_tracing() {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
-        .with_target(false)
         .compact()
         .init();
 }
