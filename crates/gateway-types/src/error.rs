b7de588735b67a3ba43ba79dad94929ca6c87327
Krzysztof Lis <CHr15F0x@proton.me>
2022-11-25 16:55:37 +0100

    refactor: starknet gateway types into a crate

---
+++ crates/gateway-types/src/error.rs
@@ -0,0 +1,124 @@
+//! Sequencer related error types.
+// FIXME
+// use crate::rpc::v01::types::reply::ErrorCode as RpcErrorCode;
+use jsonrpsee::{core::error::Error, types::error::CallError};
+use serde::{Deserialize, Serialize};
+
+/// Sequencer errors.
+#[derive(Debug, thiserror::Error)]
+pub enum SequencerError {
+    /// Starknet specific errors.
+    #[error(transparent)]
+    StarknetError(#[from] StarknetError),
+    /// Errors directly coming from reqwest
+    #[error(transparent)]
+    ReqwestError(#[from] reqwest::Error),
+    /// Custom errors that we fidded with because the original error was either
+    /// not informative enough or bloated
+    #[error("error decoding response body: invalid error variant")]
+    InvalidStarknetErrorVariant,
+}
+
+// FIXME
+// impl From<SequencerError> for Error {
+//     fn from(e: SequencerError) -> Self {
+//         match e {
+//             SequencerError::ReqwestError(e) => Error::Call(CallError::Failed(e.into())),
+//             SequencerError::InvalidStarknetErrorVariant => Error::Call(CallError::Failed(e.into())),
+//             SequencerError::StarknetError(e) => match e.code {
+//                 StarknetErrorCode::OutOfRangeBlockHash | StarknetErrorCode::BlockNotFound
+//                     if e.message.contains("Block hash") =>
+//                 {
+//                     RpcErrorCode::InvalidBlockId.into()
+//                 }
+//                 StarknetErrorCode::OutOfRangeContractAddress
+//                 | StarknetErrorCode::UninitializedContract => RpcErrorCode::ContractNotFound.into(),
+//                 StarknetErrorCode::OutOfRangeTransactionHash => {
+//                     RpcErrorCode::InvalidTransactionHash.into()
+//                 }
+//                 StarknetErrorCode::TransactionFailed => RpcErrorCode::InvalidCallData.into(),
+//                 StarknetErrorCode::TransactionLimitExceeded => {
+//                     Error::Call(CallError::Failed(e.into()))
+//                 }
+//                 StarknetErrorCode::EntryPointNotFound => {
+//                     RpcErrorCode::InvalidMessageSelector.into()
+//                 }
+//                 StarknetErrorCode::BlockNotFound if e.message.contains("Block number") => {
+//                     RpcErrorCode::InvalidBlockId.into()
+//                 }
+//                 StarknetErrorCode::InvalidContractDefinition => RpcErrorCode::ContractError.into(),
+//                 StarknetErrorCode::BlockNotFound
+//                 | StarknetErrorCode::SchemaValidationError
+//                 | StarknetErrorCode::MalformedRequest
+//                 | StarknetErrorCode::UnsupportedSelectorForFee
+//                 | StarknetErrorCode::OutOfRangeBlockHash
+//                 | StarknetErrorCode::NotPermittedContract
+//                 | StarknetErrorCode::InvalidTransactionNonce
+//                 | StarknetErrorCode::OutOfRangeFee
+//                 | StarknetErrorCode::InvalidTransactionVersion
+//                 | StarknetErrorCode::InvalidProgram => Error::Call(CallError::Failed(e.into())),
+//                 StarknetErrorCode::UndeclaredClass => RpcErrorCode::InvalidContractClassHash.into(),
+//             },
+//         }
+//     }
+// }
+
+/// Used for deserializing specific Starknet sequencer error data.
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+pub struct StarknetError {
+    pub code: StarknetErrorCode,
+    pub message: String,
+    // The `problems` field is intentionally omitted here
+    // Let's deserialize it if it proves necessary
+}
+
+impl std::error::Error for StarknetError {}
+
+impl std::fmt::Display for StarknetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+/// Represents starknet specific error codes reported by the sequencer.
+#[derive(Copy, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(deny_unknown_fields)]
+pub enum StarknetErrorCode {
+    #[serde(rename = "StarknetErrorCode.BLOCK_NOT_FOUND")]
+    BlockNotFound,
+    #[serde(rename = "StarknetErrorCode.ENTRY_POINT_NOT_FOUND_IN_CONTRACT")]
+    EntryPointNotFound,
+    #[serde(rename = "StarknetErrorCode.OUT_OF_RANGE_CONTRACT_ADDRESS")]
+    OutOfRangeContractAddress,
+    #[serde(rename = "StarkErrorCode.SCHEMA_VALIDATION_ERROR")]
+    SchemaValidationError,
+    #[serde(rename = "StarknetErrorCode.TRANSACTION_FAILED")]
+    TransactionFailed,
+    #[serde(rename = "StarknetErrorCode.UNINITIALIZED_CONTRACT")]
+    UninitializedContract,
+    #[serde(rename = "StarknetErrorCode.OUT_OF_RANGE_BLOCK_HASH")]
+    OutOfRangeBlockHash,
+    #[serde(rename = "StarknetErrorCode.OUT_OF_RANGE_TRANSACTION_HASH")]
+    OutOfRangeTransactionHash,
+    #[serde(rename = "StarkErrorCode.MALFORMED_REQUEST")]
+    MalformedRequest,
+    #[serde(rename = "StarknetErrorCode.UNSUPPORTED_SELECTOR_FOR_FEE")]
+    UnsupportedSelectorForFee,
+    #[serde(rename = "StarknetErrorCode.INVALID_CONTRACT_DEFINITION")]
+    InvalidContractDefinition,
+    #[serde(rename = "StarknetErrorCode.NON_PERMITTED_CONTRACT")]
+    NotPermittedContract,
+    #[serde(rename = "StarknetErrorCode.UNDECLARED_CLASS")]
+    UndeclaredClass,
+    /// May be returned by the transaction write api.
+    #[serde(rename = "StarknetErrorCode.TRANSACTION_LIMIT_EXCEEDED")]
+    TransactionLimitExceeded,
+    #[serde(rename = "StarknetErrorCode.INVALID_TRANSACTION_NONCE")]
+    InvalidTransactionNonce,
+    #[serde(rename = "StarknetErrorCode.OUT_OF_RANGE_FEE")]
+    OutOfRangeFee,
+    #[serde(rename = "StarknetErrorCode.INVALID_TRANSACTION_VERSION")]
+    InvalidTransactionVersion,
+    #[serde(rename = "StarknetErrorCode.INVALID_PROGRAM")]
+    InvalidProgram,
+}
---
+++ crates/gateway-types/src/request.rs
@@ -0,0 +1,234 @@
+//! Structures used for serializing requests to Starkware's sequencer REST API.
+use pathfinder_common::{CallParam, ContractAddress, EntryPoint, Fee, TransactionSignatureElem};
+
+pub mod contract {
+    use pathfinder_common::{ByteCodeOffset, EntryPoint};
+    use stark_hash::StarkHash;
+    use std::fmt;
+
+    #[derive(Copy, Clone, Debug, serde::Deserialize, serde::Serialize, PartialEq, Hash, Eq)]
+    #[serde(deny_unknown_fields)]
+    pub enum EntryPointType {
+        #[serde(rename = "EXTERNAL")]
+        External,
+        #[serde(rename = "L1_HANDLER")]
+        L1Handler,
+        #[serde(rename = "CONSTRUCTOR")]
+        Constructor,
+    }
+
+    impl fmt::Display for EntryPointType {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            use EntryPointType::*;
+            f.pad(match self {
+                External => "EXTERNAL",
+                L1Handler => "L1_HANDLER",
+                Constructor => "CONSTRUCTOR",
+            })
+        }
+    }
+
+    #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+    #[serde(deny_unknown_fields)]
+    pub struct SelectorAndOffset {
+        pub selector: EntryPoint,
+        pub offset: ByteCodeOffset,
+    }
+
+    // FIXME
+    // impl From<crate::rpc::v02::types::class::ContractEntryPoint> for SelectorAndOffset {
+    //     fn from(entry_point: crate::rpc::v02::types::class::ContractEntryPoint) -> Self {
+    //         Self {
+    //             selector: EntryPoint(entry_point.selector),
+    //             offset: ByteCodeOffset(StarkHash::from_u64(entry_point.offset)),
+    //         }
+    //     }
+    // }
+}
+
+pub mod add_transaction {
+    use super::contract::{EntryPointType, SelectorAndOffset};
+    use super::{CallParam, ContractAddress, EntryPoint, Fee, TransactionSignatureElem};
+    use pathfinder_common::{
+        ClassHash, ConstructorParam, ContractAddressSalt, TransactionNonce, TransactionVersion,
+    };
+    use pathfinder_serde::{
+        CallParamAsDecimalStr, ConstructorParamAsDecimalStr, FeeAsHexStr,
+        TransactionSignatureElemAsDecimalStr, TransactionVersionAsHexStr,
+    };
+    use serde_with::serde_as;
+    use std::collections::HashMap;
+
+    /// Definition of a contract.
+    ///
+    /// This is somewhat different compared to the contract definition we're using
+    /// for class hash calculation. The actual program contents are not relevant
+    /// for us, and they are sent as a gzip + base64 encoded string via the API.
+    #[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+    pub struct ContractDefinition {
+        // gzip + base64 encoded JSON of the compiled contract JSON
+        pub program: String,
+        pub entry_points_by_type: HashMap<EntryPointType, Vec<SelectorAndOffset>>,
+        pub abi: Option<serde_json::Value>,
+    }
+
+    // FIXME
+    // impl TryFrom<crate::rpc::v02::types::ContractClass> for ContractDefinition {
+    //     type Error = serde_json::Error;
+
+    //     fn try_from(c: crate::rpc::v02::types::ContractClass) -> Result<Self, Self::Error> {
+    //         let abi = match c.abi {
+    //             Some(abi) => Some(serde_json::to_value(abi)?),
+    //             None => None,
+    //         };
+    //         let mut entry_points: HashMap<EntryPointType, Vec<SelectorAndOffset>> =
+    //             Default::default();
+    //         entry_points.insert(
+    //             EntryPointType::Constructor,
+    //             c.entry_points_by_type
+    //                 .constructor
+    //                 .into_iter()
+    //                 .map(Into::into)
+    //                 .collect(),
+    //         );
+    //         entry_points.insert(
+    //             EntryPointType::External,
+    //             c.entry_points_by_type
+    //                 .external
+    //                 .into_iter()
+    //                 .map(Into::into)
+    //                 .collect(),
+    //         );
+    //         entry_points.insert(
+    //             EntryPointType::L1Handler,
+    //             c.entry_points_by_type
+    //                 .l1_handler
+    //                 .into_iter()
+    //                 .map(Into::into)
+    //                 .collect(),
+    //         );
+
+    //         Ok(Self {
+    //             program: c.program,
+    //             entry_points_by_type: entry_points,
+    //             abi,
+    //         })
+    //     }
+    // }
+
+    /// Contract deployment transaction details.
+    #[serde_as]
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    pub struct Deploy {
+        // Transacion properties
+        #[serde_as(as = "TransactionVersionAsHexStr")]
+        pub version: TransactionVersion,
+
+        pub contract_address_salt: ContractAddressSalt,
+        pub contract_definition: ContractDefinition,
+        #[serde_as(as = "Vec<ConstructorParamAsDecimalStr>")]
+        pub constructor_calldata: Vec<ConstructorParam>,
+    }
+
+    /// Account deployment transaction details.
+    #[serde_as]
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    pub struct DeployAccount {
+        // Transaction properties
+        #[serde_as(as = "TransactionVersionAsHexStr")]
+        pub version: TransactionVersion,
+
+        #[serde_as(as = "FeeAsHexStr")]
+        pub max_fee: Fee,
+        #[serde_as(as = "Vec<TransactionSignatureElemAsDecimalStr>")]
+        pub signature: Vec<TransactionSignatureElem>,
+        pub nonce: TransactionNonce,
+
+        pub class_hash: ClassHash,
+        pub contract_address_salt: ContractAddressSalt,
+        #[serde_as(as = "Vec<CallParamAsDecimalStr>")]
+        pub constructor_calldata: Vec<CallParam>,
+    }
+
+    /// Invoke contract transaction details.
+    #[serde_as]
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    pub struct InvokeFunction {
+        // Transacion properties
+        #[serde_as(as = "TransactionVersionAsHexStr")]
+        pub version: TransactionVersion,
+
+        // AccountTransaction properties
+        #[serde_as(as = "FeeAsHexStr")]
+        pub max_fee: Fee,
+        #[serde_as(as = "Vec<TransactionSignatureElemAsDecimalStr>")]
+        pub signature: Vec<TransactionSignatureElem>,
+        pub nonce: Option<TransactionNonce>,
+
+        pub contract_address: ContractAddress,
+        pub entry_point_selector: Option<EntryPoint>,
+        #[serde_as(as = "Vec<CallParamAsDecimalStr>")]
+        pub calldata: Vec<CallParam>,
+    }
+
+    /// Declare transaction details.
+    #[serde_as]
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    pub struct Declare {
+        // Transacion properties
+        #[serde_as(as = "TransactionVersionAsHexStr")]
+        pub version: TransactionVersion,
+
+        // AccountTransaction properties -- except for nonce which is non-optional here
+        #[serde_as(as = "FeeAsHexStr")]
+        pub max_fee: Fee,
+        #[serde_as(as = "Vec<TransactionSignatureElemAsDecimalStr>")]
+        pub signature: Vec<TransactionSignatureElem>,
+
+        pub contract_class: ContractDefinition,
+        pub sender_address: ContractAddress,
+        pub nonce: TransactionNonce,
+    }
+
+    /// Add transaction API operation.
+    ///
+    /// This adds the "type" attribute to the JSON request according the type of
+    /// the transaction (invoke or deploy).
+    #[derive(Debug, serde::Deserialize, serde::Serialize)]
+    #[serde(tag = "type")]
+    pub enum AddTransaction {
+        #[serde(rename = "INVOKE_FUNCTION")]
+        Invoke(InvokeFunction),
+        #[serde(rename = "DEPLOY")]
+        Deploy(Deploy),
+        #[serde(rename = "DECLARE")]
+        Declare(Declare),
+        #[serde(rename = "DEPLOY_ACCOUNT")]
+        DeployAccount(DeployAccount),
+    }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn test_deploy() {
+            let json = include_bytes!("../fixtures/add-transaction/deploy_transaction.json");
+            let _deploy = serde_json::from_slice::<AddTransaction>(json).unwrap();
+        }
+
+        #[test]
+        fn test_deploy_openzeppelin_account() {
+            let json =
+                include_bytes!("../fixtures/add-transaction/deploy_openzeppelin_account.json");
+            let _deploy = serde_json::from_slice::<AddTransaction>(json).unwrap();
+        }
+
+        #[test]
+        fn test_invoke_with_signature() {
+            let json =
+                include_bytes!("../fixtures/add-transaction/invoke_contract_with_signature.json");
+            let _invoke = serde_json::from_slice::<AddTransaction>(json).unwrap();
+        }
+    }
+}
