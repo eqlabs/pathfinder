d508b6562b3ca1268dc8a36f9183ac68f3226e83
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2022-11-29 16:55:17 +0200

    fix: docs and dangling ethereum types

--- crates/ethereum/src/log/fetch.rs
+++ crates/ethereum/src/log/fetch.rs
@@ -6,29 +6,12 @@ mod forward;
 
 pub use forward::*;
 
-/// May contain one of two types of [MetaLog].
-///
-/// Used by [BackwardLogFetcher].
-#[derive(Debug, PartialEq, Eq, Clone)]
-pub enum EitherMetaLog<L, R>
-where
-    L: MetaLog + PartialEq + std::fmt::Debug + Clone,
-    R: MetaLog + PartialEq + std::fmt::Debug + Clone,
-{
-    Left(L),
-    Right(R),
-}
-
-/// Trait used by [LogFetcher] and indirectly by [BackwardLogFetcher] (via [EitherMetaLog]).
+/// Trait used by [LogFetcher].
 ///
 /// Contains metadata for a log such as its point-of-origin on L1 and it's
 /// emitting contract and event signature.
 ///
-/// Implemented for the four Starknet log types,
-///     - [StateUpdateLog]
-///     - [StateTransitionFactLog]
-///     - [MemoryPagesHashesLog]
-///     - [MemoryPageFactContinuousLog]
+/// Implemented by [StateUpdateLog]
 pub trait MetaLog: TryFrom<web3::types::Log, Error = anyhow::Error> {
     fn signature() -> H256;
 
