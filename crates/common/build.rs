67bab9e6ba6efde8695b9345e716058e0a8b08fc
Krisztian Kovacs <krisztian@equilibrium.co>
2022-12-07 11:24:45 +0100

    fix(build.rs): disable all unnecessary output from vergen

--- crates/common/build.rs
+++ crates/common/build.rs
@@ -7,5 +7,11 @@ pub fn main() {
     let mut config = vergen::Config::default();
     *config.git_mut().semver_kind_mut() = vergen::SemverKind::Lightweight;
     *config.git_mut().semver_dirty_mut() = Some("-dirty");
+    *config.git_mut().branch_mut() = false;
+    *config.git_mut().commit_author_mut() = false;
+    *config.git_mut().commit_count_mut() = false;
+    *config.git_mut().commit_message_mut() = false;
+    *config.git_mut().commit_timestamp_mut() = false;
+    *config.git_mut().sha_mut() = false;
     vergen::vergen(config).expect("vergen failed; this is probably due to missing .git directory");
 }
