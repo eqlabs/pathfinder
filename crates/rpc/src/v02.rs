0cc82e6cf448e9488c2da6652d7366340d2c592a
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-01-24 16:38:44 +0200

    refactor(rpc): reduce stuttering

--- crates/rpc/src/v02.rs
+++ crates/rpc/src/v02.rs
@@ -9,77 +9,62 @@ pub mod types;
 /// Registers all methods for the v0.2 RPC API
 pub fn register_methods(context: RpcContext) -> anyhow::Result<Methods> {
     let methods = crate::module::Module::new(context)
-        .register_method("starknet_call", method::call::call)?
-        .register_method_with_no_input("starknet_chainId", method::chain_id::chain_id)?
+        .register_method("starknet_call", method::call)?
+        .register_method_with_no_input("starknet_chainId", method::chain_id)?
         .register_method(
             "starknet_getBlockWithTxHashes",
-            method::get_block::get_block_with_tx_hashes,
+            method::get_block_with_tx_hashes,
         )?
-        .register_method(
-            "starknet_getBlockWithTxs",
-            method::get_block::get_block_with_txs,
-        )?
-        .register_method("starknet_getClass", method::get_class::get_class)?
-        .register_method("starknet_getClassAt", method::get_class_at::get_class_at)?
-        .register_method(
-            "starknet_getClassHashAt",
-            method::get_class_hash_at::get_class_hash_at,
-        )?
-        .register_method("starknet_getEvents", method::get_events::get_events)?
-        .register_method("starknet_estimateFee", method::estimate_fee::estimate_fee)?
-        .register_method("starknet_getNonce", method::get_nonce::get_nonce)?
+        .register_method("starknet_getBlockWithTxs", method::get_block_with_txs)?
+        .register_method("starknet_getClass", method::get_class)?
+        .register_method("starknet_getClassAt", method::get_class_at)?
+        .register_method("starknet_getClassHashAt", method::get_class_hash_at)?
+        .register_method("starknet_getEvents", method::get_events)?
+        .register_method("starknet_estimateFee", method::estimate_fee)?
+        .register_method("starknet_getNonce", method::get_nonce)?
         .register_method_with_no_input(
             "starknet_pendingTransactions",
-            method::pending_transactions::pending_transactions,
+            method::pending_transactions,
         )?
-        .register_method(
-            "starknet_getStateUpdate",
-            method::get_state_update::get_state_update,
-        )?
-        .register_method(
-            "starknet_getStorageAt",
-            method::get_storage_at::get_storage_at,
-        )?
+        .register_method("starknet_getStateUpdate", method::get_state_update)?
+        .register_method("starknet_getStorageAt", method::get_storage_at)?
         .register_method(
             "starknet_getTransactionByHash",
-            method::get_transaction_by_hash::get_transaction_by_hash,
+            method::get_transaction_by_hash,
         )?
         .register_method(
             "starknet_getTransactionByBlockIdAndIndex",
-            method::get_transaction_by_block_id_and_index::get_transaction_by_block_id_and_index,
+            method::get_transaction_by_block_id_and_index,
         )?
         .register_method(
             "starknet_getTransactionReceipt",
-            method::get_transaction_receipt::get_transaction_receipt,
+            method::get_transaction_receipt,
         )?
-        .register_method_with_no_input("starknet_syncing", method::syncing::syncing)?
+        .register_method_with_no_input("starknet_syncing", method::syncing)?
         .register_method(
             "starknet_getBlockTransactionCount",
-            method::get_block_transaction_count::get_block_transaction_count,
+            method::get_block_transaction_count,
         )?
         .register_method_with_no_input(
             "starknet_blockHashAndNumber",
-            method::block_hash_and_number::block_hash_and_number,
+            method::block_hash_and_number,
         )?
-        .register_method_with_no_input(
-            "starknet_blockNumber",
-            method::block_hash_and_number::block_number,
-        )?
+        .register_method_with_no_input("starknet_blockNumber", method::block_number)?
         .register_method(
             "starknet_addInvokeTransaction",
-            method::add_invoke_transaction::add_invoke_transaction,
+            method::add_invoke_transaction,
         )?
         .register_method(
             "starknet_addDeclareTransaction",
-            method::add_declare_transaction::add_declare_transaction,
+            method::add_declare_transaction,
         )?
         .register_method(
             "starknet_addDeployTransaction",
-            method::add_deploy_transaction::add_deploy_transaction,
+            method::add_deploy_transaction,
         )?
         .register_method(
             "starknet_addDeployAccountTransaction",
-            method::add_deploy_account_transaction::add_deploy_account_transaction,
+            method::add_deploy_account_transaction,
         )?
         .register_method(
             "pathfinder_getProof",
