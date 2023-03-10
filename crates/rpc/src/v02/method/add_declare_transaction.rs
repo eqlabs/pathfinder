4d9fb34447b5bd8be03c22db5c230a51accebebb
Krzysztof Lis <klis33@gmail.com>
2023-03-10 18:41:24 +0100

    test(rpc/add_declare_transaction): add v2 parsing case

--- crates/rpc/src/v02/method/add_declare_transaction.rs
+++ crates/rpc/src/v02/method/add_declare_transaction.rs
@@ -136,85 +136,167 @@ mod tests {
             zstd::decode_all(starknet_gateway_test_fixtures::zstd_compressed_contracts::CAIRO_0_11_SIERRA).unwrap()
         };
 
+        pub static ref SIERRA_CLASS_JSON: String = {
+            serde_json::to_string(&*SIERRA_CLASS).unwrap()
+        };
+
         pub static ref SIERRA_CLASS: SierraContractClass = {
             ContractClass::from_definition_bytes(&SIERRA_CLASS_DEFINITION_JSON).unwrap().as_sierra().unwrap()
         };
     }
 
     mod parsing {
-        use crate::v02::types::request::BroadcastedDeclareTransactionV0V1;
+        mod v1 {
+            use super::super::*;
+            use crate::v02::types::request::BroadcastedDeclareTransactionV0V1;
 
-        use super::*;
+            fn test_declare_txn() -> Transaction {
+                Transaction::Declare(BroadcastedDeclareTransaction::V0V1(
+                    BroadcastedDeclareTransactionV0V1 {
+                        max_fee: Fee(ethers::types::H128::from_low_u64_be(1)),
+                        version: TransactionVersion::ONE,
+                        signature: vec![],
+                        nonce: TransactionNonce(Felt::ZERO),
+                        contract_class: CONTRACT_CLASS.clone(),
+                        sender_address: ContractAddress::new_or_panic(Felt::from_u64(1)),
+                    },
+                ))
+            }
 
-        fn test_declare_txn() -> Transaction {
-            // Fixme v0.11.0 only allow v2?
-            Transaction::Declare(BroadcastedDeclareTransaction::V0V1(
-                BroadcastedDeclareTransactionV0V1 {
-                    max_fee: Fee(ethers::types::H128::from_low_u64_be(1)),
-                    version: TransactionVersion::ONE,
-                    signature: vec![],
-                    nonce: TransactionNonce(Felt::ZERO),
-                    contract_class: CONTRACT_CLASS.clone(),
-                    sender_address: ContractAddress::new_or_panic(Felt::from_u64(1)),
-                },
-            ))
+            #[test]
+            fn positional_args() {
+                use jsonrpsee::types::Params;
+
+                let positional = format!(
+                    r#"[
+                        {{
+                            "type": "DECLARE",
+                            "version": "0x1",
+                            "max_fee": "0x1",
+                            "signature": [],
+                            "nonce": "0x0",
+                            "contract_class": {},
+                            "sender_address": "0x1"
+                        }}
+                    ]"#,
+                    CONTRACT_CLASS_JSON.clone()
+                );
+                let positional = Params::new(Some(&positional));
+
+                let input = positional.parse::<AddDeclareTransactionInput>().unwrap();
+                let expected = AddDeclareTransactionInput {
+                    declare_transaction: test_declare_txn(),
+                    token: None,
+                };
+                assert_eq!(input, expected);
+            }
+
+            #[test]
+            fn named_args() {
+                use jsonrpsee::types::Params;
+
+                let named = format!(
+                    r#"{{
+                        "declare_transaction": {{
+                            "type": "DECLARE",
+                            "version": "0x1",
+                            "max_fee": "0x1",
+                            "signature": [],
+                            "nonce": "0x0",
+                            "contract_class": {},
+                            "sender_address": "0x1"
+                        }},
+                        "token": "token"
+                    }}"#,
+                    CONTRACT_CLASS_JSON.clone()
+                );
+                let named = Params::new(Some(&named));
+
+                let input = named.parse::<AddDeclareTransactionInput>().unwrap();
+                let expected = AddDeclareTransactionInput {
+                    declare_transaction: test_declare_txn(),
+                    token: Some("token".to_owned()),
+                };
+                assert_eq!(input, expected);
+            }
         }
 
-        #[test]
-        fn positional_args() {
-            use jsonrpsee::types::Params;
+        mod v2 {
+            use super::super::*;
+            use crate::v02::types::request::BroadcastedDeclareTransactionV2;
 
-            let positional = format!(
-                r#"[
-                    {{
-                        "type": "DECLARE",
-                        "version": "0x1",
-                        "max_fee": "0x1",
-                        "signature": [],
-                        "nonce": "0x0",
-                        "contract_class": {},
-                        "sender_address": "0x1"
-                    }}
-                ]"#,
-                CONTRACT_CLASS_JSON.clone()
-            );
-            let positional = Params::new(Some(&positional));
+            fn test_declare_txn() -> Transaction {
+                Transaction::Declare(BroadcastedDeclareTransaction::V2(
+                    BroadcastedDeclareTransactionV2 {
+                        max_fee: Fee(ethers::types::H128::from_low_u64_be(1)),
+                        version: TransactionVersion::TWO,
+                        signature: vec![],
+                        nonce: TransactionNonce(Felt::ZERO),
+                        contract_class: SIERRA_CLASS.clone(),
+                        sender_address: ContractAddress::new_or_panic(Felt::from_u64(1)),
+                        compiled_class_hash: Some(CasmHash(Felt::from_u64(1))),
+                    },
+                ))
+            }
 
-            let input = positional.parse::<AddDeclareTransactionInput>().unwrap();
-            let expected = AddDeclareTransactionInput {
-                declare_transaction: test_declare_txn(),
-                token: None,
-            };
-            assert_eq!(input, expected);
-        }
+            #[test]
+            fn positional_args() {
+                use jsonrpsee::types::Params;
 
-        #[test]
-        fn named_args() {
-            use jsonrpsee::types::Params;
+                let positional = format!(
+                    r#"[
+                        {{
+                            "type": "DECLARE",
+                            "version": "0x2",
+                            "max_fee": "0x1",
+                            "signature": [],
+                            "nonce": "0x0",
+                            "contract_class": {},
+                            "sender_address": "0x1",
+                            "compiled_class_hash": "0x1"
+                        }}
+                    ]"#,
+                    SIERRA_CLASS_JSON.clone()
+                );
+                let positional = Params::new(Some(&positional));
 
-            let named = format!(
-                r#"{{
-                    "declare_transaction": {{
-                        "type": "DECLARE",
-                        "version": "0x1",
-                        "max_fee": "0x1",
-                        "signature": [],
-                        "nonce": "0x0",
-                        "contract_class": {},
-                        "sender_address": "0x1"
-                    }},
-                    "token": "token"
-                }}"#,
-                CONTRACT_CLASS_JSON.clone()
-            );
-            let named = Params::new(Some(&named));
+                let input = positional.parse::<AddDeclareTransactionInput>().unwrap();
+                let expected = AddDeclareTransactionInput {
+                    declare_transaction: test_declare_txn(),
+                    token: None,
+                };
+                pretty_assertions::assert_eq!(input, expected);
+            }
 
-            let input = named.parse::<AddDeclareTransactionInput>().unwrap();
-            let expected = AddDeclareTransactionInput {
-                declare_transaction: test_declare_txn(),
-                token: Some("token".to_owned()),
-            };
-            assert_eq!(input, expected);
+            #[test]
+            fn named_args() {
+                use jsonrpsee::types::Params;
+
+                let named = format!(
+                    r#"{{
+                        "declare_transaction": {{
+                            "type": "DECLARE",
+                            "version": "0x2",
+                            "max_fee": "0x1",
+                            "signature": [],
+                            "nonce": "0x0",
+                            "contract_class": {},
+                            "sender_address": "0x1",
+                            "compiled_class_hash": "0x1"
+                        }},
+                        "token": "token"
+                    }}"#,
+                    SIERRA_CLASS_JSON.clone()
+                );
+                let named = Params::new(Some(&named));
+
+                let input = named.parse::<AddDeclareTransactionInput>().unwrap();
+                let expected = AddDeclareTransactionInput {
+                    declare_transaction: test_declare_txn(),
+                    token: Some("token".to_owned()),
+                };
+                pretty_assertions::assert_eq!(input, expected);
+            }
         }
     }
 
@@ -249,11 +331,44 @@ mod tests {
     }
 
     #[test_log::test(tokio::test)]
+    async fn invalid_contract_definition_v2() {
+        let context = RpcContext::for_tests_on(pathfinder_common::Chain::Integration);
+
+        let invalid_contract_class = SierraContractClass {
+            sierra_program: vec![],
+            ..SIERRA_CLASS.clone()
+        };
+
+        let declare_transaction = Transaction::Declare(BroadcastedDeclareTransaction::V2(
+            BroadcastedDeclareTransactionV2 {
+                version: TransactionVersion::TWO,
+                max_fee: Fee(ethers::types::H128::from_low_u64_be(u64::MAX)),
+                signature: vec![],
+                nonce: TransactionNonce(Default::default()),
+                contract_class: invalid_contract_class,
+                sender_address: ContractAddress::new_or_panic(Felt::from_u64(1)),
+                // Taken from
+                // https://external.integration.starknet.io/feeder_gateway/get_state_update?blockNumber=283364
+                compiled_class_hash: Some(CasmHash::new_or_panic(felt!(
+                    "0x711c0c3e56863e29d3158804aac47f424241eda64db33e2cc2999d60ee5105"
+                ))),
+            },
+        ));
+
+        let input = AddDeclareTransactionInput {
+            declare_transaction,
+            token: None,
+        };
+        let error = add_declare_transaction(context, input).await.unwrap_err();
+        // FIXME This should be a specific Starknet gateway variant, once the gateway is fixed
+        assert_matches::assert_matches!(error, AddDeclareTransactionError::Internal(_));
+    }
+
+    #[test_log::test(tokio::test)]
     #[ignore = "gateway 429"]
-    async fn successful_declare() {
+    async fn successful_declare_v1() {
         let context = RpcContext::for_tests();
 
-        // Fixme v0.11.0 only allow v2?
         let declare_transaction = Transaction::Declare(BroadcastedDeclareTransaction::V0V1(
             BroadcastedDeclareTransactionV0V1 {
                 version: TransactionVersion::ONE,
@@ -290,13 +405,12 @@ mod tests {
         let declare_transaction = Transaction::Declare(BroadcastedDeclareTransaction::V2(
             BroadcastedDeclareTransactionV2 {
                 version: TransactionVersion::TWO,
-                // FIXME No idea what the real max is
                 max_fee: Fee(ethers::types::H128::from_low_u64_be(u64::MAX)),
                 signature: vec![],
                 nonce: TransactionNonce(Default::default()),
                 contract_class: SIERRA_CLASS.clone(),
                 sender_address: ContractAddress::new_or_panic(Felt::from_u64(1)),
-                // Taken from here :p
+                // Taken from
                 // https://external.integration.starknet.io/feeder_gateway/get_state_update?blockNumber=283364
                 compiled_class_hash: Some(CasmHash::new_or_panic(felt!(
                     "0x711c0c3e56863e29d3158804aac47f424241eda64db33e2cc2999d60ee5105"
