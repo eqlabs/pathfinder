a6af7ed4ff47650bca9f554f8c7cd9b65519e9c8
Krisztian Kovacs <krisztian@equilibrium.co>
2023-03-16 11:08:37 +0100

    chore(Dockerfile): add protobuf-compiler package

--- build/prepare.sh
+++ build/prepare.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 if [[ "${TARGETARCH}" == "amd64" ]]; then
     apt-get update
-    DEBIAN_FRONTEND=noninteractive apt-get install -y libssl-dev
+    DEBIAN_FRONTEND=noninteractive apt-get install -y libssl-dev protobuf-compiler
 elif [[ "${TARGETARCH}" == "arm64" ]]; then
     echo "deb [arch=arm64] http://deb.debian.org/debian bullseye main" >>/etc/apt/sources.list
     apt-get update
-    DEBIAN_FRONTEND=noninteractive apt-get install -y libssl-dev gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross
+    DEBIAN_FRONTEND=noninteractive apt-get install -y libssl-dev protobuf-compiler gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross
     apt-get download libssl-dev:arm64 libssl1.1:arm64
     mkdir -p /build/sysroot
     dpkg -x libssl-dev_*.deb /build/sysroot/
