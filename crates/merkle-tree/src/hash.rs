01b96506874870f20ea786929f48ebf7a9c2026c
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-04-05 08:11:49 +0200

    refactor: move merkle hash into own file

---
+++ crates/merkle-tree/src/hash.rs
@@ -0,0 +1,26 @@
+//! Contains the [Hash] trait and implementations thereof for the [Pedersen](PedersenHash) hash and [Poseidon](PoseidonHash).
+
+use stark_hash::Felt;
+
+/// Hashing function used by a particular merkle tree implementation.
+pub trait Hash {
+    fn hash(a: Felt, b: Felt) -> Felt;
+}
+
+/// Implements [Hash] for the [StarkNet Pedersen hash](stark_hash::stark_hash).
+#[derive(Debug, Clone, Copy)]
+pub struct PedersenHash {}
+
+impl Hash for PedersenHash {
+    fn hash(a: Felt, b: Felt) -> Felt {
+        stark_hash::stark_hash(a, b)
+    }
+}
+
+/// Implements [Hash] for the [StarkNet Poseidon hash](stark_poseidon::poseidon_hash).
+pub struct PoseidonHash;
+impl crate::Hash for PoseidonHash {
+    fn hash(a: Felt, b: Felt) -> Felt {
+        stark_poseidon::poseidon_hash(a.into(), b.into()).into()
+    }
+}
--- crates/merkle-tree/src/lib.rs
+++ crates/merkle-tree/src/lib.rs
@@ -1,33 +1,11 @@
-use stark_hash::Felt;
-
 pub mod contract_state;
 pub mod merkle_node;
 pub mod merkle_tree;
 
 mod class;
 mod contract;
+mod hash;
+
 pub use class::ClassCommitmentTree;
 pub use contract::{ContractsStateTree, StorageCommitmentTree};
-
-/// Hashing function used by a particular merkle tree implementation.
-pub trait Hash {
-    fn hash(left: Felt, right: Felt) -> Felt;
-}
-
-/// Implements [Hash] for the [StarkNet Pedersen hash](stark_hash::stark_hash).
-#[derive(Debug, Clone, Copy)]
-pub struct PedersenHash {}
-
-impl Hash for PedersenHash {
-    fn hash(left: Felt, right: Felt) -> Felt {
-        stark_hash::stark_hash(left, right)
-    }
-}
-
-/// Implements [Hash] for the [StarkNet Poseidon hash](stark_poseidon::poseidon_hash).
-struct PoseidonHash;
-impl crate::Hash for PoseidonHash {
-    fn hash(left: Felt, right: Felt) -> Felt {
-        stark_poseidon::poseidon_hash(left.into(), right.into()).into()
-    }
-}
+pub use hash::{Hash, PedersenHash, PoseidonHash};
