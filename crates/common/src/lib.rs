85b3a7ea55567d0a286e01449517c8efe7d050b9
Krzysztof Lis <klis33@gmail.com>
2023-04-18 09:19:58 +0200

    test(cors): add a basic integration test for preflight

--- crates/common/src/lib.rs
+++ crates/common/src/lib.rs
@@ -548,6 +548,21 @@ pub enum AllowedOrigins {
     List(Vec<String>),
 }
 
+impl<S> From<S> for AllowedOrigins
+where
+    S: ToString,
+{
+    fn from(value: S) -> Self {
+        let s = value.to_string();
+
+        if s == "*" {
+            Self::Any
+        } else {
+            Self::List(vec![s])
+        }
+    }
+}
+
 /// See:
 /// <https://github.com/starkware-libs/cairo-lang/blob/64a7f6aed9757d3d8d6c28bd972df73272b0cb0a/src/starkware/starknet/public/abi.py#L21-L26>
 pub fn truncated_keccak(mut plain: [u8; 32]) -> Felt {
--- crates/rpc/src/middleware/cors.rs
+++ crates/rpc/src/middleware/cors.rs
@@ -19,8 +19,56 @@ pub fn with_allowed_origins(allowed_origins: AllowedOrigins) -> CorsLayer {
 
 #[cfg(test)]
 mod tests {
+    use crate::{context::RpcContext, RpcServer};
+    use http::HeaderValue;
+
     #[tokio::test]
-    async fn cors_works() {
-        // unimplemented!();
+    async fn preflight() {
+        for (allowed, expected, line) in [
+            // Successful
+            (Some("*"), Some("*"), line!()),
+            (Some("http://a.com"), Some("http://a.com"), line!()),
+            // Not on the list
+            (Some("http://b.com"), None, line!()),
+            // Disabled
+            (None, None, line!()),
+        ] {
+            let context = RpcContext::for_tests();
+            let server = RpcServer::new("127.0.0.1:0".parse().unwrap(), context);
+            let server = match allowed {
+                Some(allowed) => server.with_cors(allowed.into()),
+                None => server,
+            };
+
+            let (_server_handle, address) = server.run().await.unwrap();
+
+            let resp = reqwest::Client::new()
+                .request(reqwest::Method::OPTIONS, format!("http://{address}"))
+                .header("Access-Control-Request-Headers", "content-type")
+                .header("Access-Control-Request-Method", "POST")
+                .header("Origin", "http://a.com")
+                .body("")
+                .send()
+                .await
+                .unwrap();
+
+            let h = resp.headers();
+
+            assert_eq!(
+                h.get("access-control-allow-headers"),
+                allowed.and(Some(&HeaderValue::from_static("content-type"))),
+                "line: {line}"
+            );
+            assert_eq!(
+                h.get("access-control-allow-methods"),
+                allowed.and(Some(&HeaderValue::from_static("POST"))),
+                "line: {line}"
+            );
+            assert_eq!(
+                h.get("access-control-allow-origin"),
+                expected.map(HeaderValue::from_static).as_ref(),
+                "line: {line}"
+            );
+        }
     }
 }
