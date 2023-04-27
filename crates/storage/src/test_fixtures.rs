0d1b957b849f0747ad3737d98ad41c7549d4417f
Krisztian Kovacs <krisztian@equilibrium.co>
2023-04-27 14:35:27 +0200

    fix(crates/storage): fix consistency in test fixtures
    All required tables are now initialized (including ContractCodeTable) and
    the new `insert_canonical_state_diff()` function is used to update
    flattened state.
    
    State commitments are now properly calculated based on storage and class
    commitments and link previous blocks are linked.

--- crates/storage/src/test_fixtures.rs
+++ crates/storage/src/test_fixtures.rs
@@ -11,9 +11,9 @@ use crate::{
     {StarknetBlock, Storage},
 };
 use pathfinder_common::{
-    CasmHash, ClassHash, ContractAddress, ContractNonce, GasPrice, SequencerAddress, SierraHash,
-    StarknetBlockHash, StarknetBlockNumber, StarknetBlockTimestamp, StateCommitment,
-    StorageAddress, StorageValue,
+    CasmHash, ClassCommitment, ClassHash, ContractAddress, ContractNonce, GasPrice,
+    SequencerAddress, SierraHash, StarknetBlockHash, StarknetBlockNumber, StarknetBlockTimestamp,
+    StateCommitment, StorageAddress, StorageCommitment, StorageValue,
 };
 use rusqlite::Transaction;
 use stark_hash::Felt;
@@ -30,13 +30,17 @@ pub(crate) use hash;
 /// Initializers for storage test fixtures.
 pub mod init {
     use super::*;
-    use crate::{StarknetBlocksTable, StarknetStateUpdatesTable};
+    use crate::{
+        state_update::insert_canonical_state_diff, CanonicalBlocksTable, ContractCodeTable,
+        StarknetBlocksTable, StarknetStateUpdatesTable,
+    };
     use pathfinder_common::{ClassCommitment, StarknetVersion, StorageCommitment};
 
     /// Inserts `n` state updates, referring to blocks with numbers `(0..n)` and hashes `("0x0".."0xn")` respectively.
     pub fn with_n_state_updates(tx: &Transaction<'_>, n: u8) -> Vec<StateUpdate> {
         (0..n)
             .map(|n| {
+                let block_number = StarknetBlockNumber::new_or_panic(n as u64);
                 StarknetBlocksTable::insert(
                     tx,
                     &StarknetBlock::nth(n),
@@ -45,7 +49,23 @@ pub mod init {
                     ClassCommitment(hash!(12, n)),
                 )
                 .unwrap();
+                CanonicalBlocksTable::insert(tx, block_number, StarknetBlockHash(hash!(n)))
+                    .unwrap();
+
                 let update = StateUpdate::with_block_hash(n);
+
+                insert_canonical_state_diff(tx, block_number, &update.state_diff).unwrap();
+
+                for declared_class in &update.state_diff.declared_contracts {
+                    ContractCodeTable::insert(tx, declared_class.class_hash, b"").unwrap();
+                    ContractCodeTable::update_block_number_if_null(
+                        tx,
+                        declared_class.class_hash,
+                        block_number,
+                    )
+                    .unwrap();
+                }
+
                 StarknetStateUpdatesTable::insert(tx, update.block_hash.unwrap(), &update).unwrap();
                 update
             })
@@ -56,10 +76,30 @@ pub mod init {
 impl StateUpdate {
     /// Creates a [`StateUpdate`] for a block with hash `0xh` filled with arbitrary data useful for testing.
     pub fn with_block_hash(h: u8) -> Self {
+        let old_root = if h > 0 {
+            StateCommitment::calculate(
+                StorageCommitment(hash!(11, h - 1)),
+                ClassCommitment(hash!(12, h - 1)),
+            )
+        } else {
+            StateCommitment(Felt::ZERO)
+        };
+        let replaced_classes = if h > 0 {
+            // contract deployed in the previous block
+            vec![ReplacedClass {
+                address: ContractAddress::new_or_panic(hash!(7, h - 1)),
+                class_hash: ClassHash(hash!(14, h)),
+            }]
+        } else {
+            vec![]
+        };
         Self {
             block_hash: Some(StarknetBlockHash(hash!(h))),
-            new_root: StateCommitment(hash!(1, h)),
-            old_root: StateCommitment(hash!(2, h)),
+            new_root: StateCommitment::calculate(
+                StorageCommitment(hash!(11, h)),
+                ClassCommitment(hash!(12, h)),
+            ),
+            old_root,
             state_diff: StateDiff {
                 storage_diffs: vec![StorageDiff {
                     address: ContractAddress::new_or_panic(hash!(3, h)),
@@ -81,10 +121,7 @@ impl StateUpdate {
                     class_hash: SierraHash(hash!(11, h)),
                     compiled_class_hash: CasmHash(hash!(12, h)),
                 }],
-                replaced_classes: vec![ReplacedClass {
-                    address: ContractAddress::new_or_panic(hash!(13, h)),
-                    class_hash: ClassHash(hash!(14, h)),
-                }],
+                replaced_classes,
             },
         }
     }
@@ -96,7 +133,10 @@ impl StarknetBlock {
         Self {
             number: StarknetBlockNumber::new(n as u64).expect("block number out of range"),
             hash: StarknetBlockHash(hash!(n)),
-            state_commmitment: StateCommitment(hash!(1, n)),
+            state_commmitment: StateCommitment::calculate(
+                StorageCommitment(hash!(11, n)),
+                ClassCommitment(hash!(12, n)),
+            ),
             timestamp: StarknetBlockTimestamp::new(n as u64 + 1000)
                 .expect("block timestamp out of range"),
             gas_price: GasPrice(n as u128 + 2000),
