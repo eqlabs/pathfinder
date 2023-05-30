f0eb0442e67b96ca67724d696db5e726c3d1b694
Sergey Melnychuk <sergey@eiger.co>
2023-05-30 14:34:34 +0200

    chore: remove redundant `Event` wrapper

--- crates/pathfinder/src/state/sync/l1.rs
+++ crates/pathfinder/src/state/sync/l1.rs
@@ -3,18 +3,10 @@ use pathfinder_ethereum::{EthereumApi, EthereumStateUpdate};
 use primitive_types::H160;
 use tokio::sync::mpsc;
 
-// TODO(SM): remove `Event` wrapper
-/// Events and queries emitted by L1 sync process.
-#[derive(Debug)]
-pub enum Event {
-    /// New L1 [update](EthereumStateUpdate).
-    Update(EthereumStateUpdate),
-}
-
 /// Syncs L1 state update logs. Emits [sync events](Event) which should be handled
 /// to update storage and respond to queries.
 pub async fn sync<T>(
-    tx_event: mpsc::Sender<Event>,
+    tx_event: mpsc::Sender<EthereumStateUpdate>,
     ethereum: T,
     chain: Chain,
     core_address: H160,
@@ -28,7 +20,7 @@ where
 
 async fn sync_impl(
     ethereum: impl EthereumApi,
-    tx_event: mpsc::Sender<Event>,
+    tx_event: mpsc::Sender<EthereumStateUpdate>,
     chain: Chain,
     core_address: H160,
 ) -> anyhow::Result<()> {
@@ -38,7 +30,7 @@ async fn sync_impl(
 
     loop {
         let state_update = ethereum.get_starknet_state(&core_address).await?;
-        tx_event.send(Event::Update(state_update)).await?;
+        tx_event.send(state_update).await?;
 
         tokio::time::sleep(head_poll_interval).await;
         continue;
