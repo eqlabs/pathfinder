fbbb18cfe88335840b1b9b150c454621b3f5fe1d
Sergey Melnychuk <sergey@eiger.co>
2023-06-16 12:59:24 +0200

    tests: add error cases for `estimate_message_fee`

--- crates/rpc/src/v03/method/estimate_message_fee.rs
+++ crates/rpc/src/v03/method/estimate_message_fee.rs
@@ -191,23 +191,189 @@ mod tests {
             block_id: BlockId::Number(BlockNumber::new_or_panic(1)),
         };
 
+        let expected = FeeEstimate {
+            gas_consumed: H256::from_str(
+                "0x000000000000000000000000000000000000000000000000000000000000479a"
+            )
+            .unwrap(),
+            gas_price: H256::from_str(
+                "0x0000000000000000000000000000000000000000000000000000000000000001"
+            )
+            .unwrap(),
+            overall_fee: H256::from_str(
+                "0x000000000000000000000000000000000000000000000000000000000000479a"
+            )
+            .unwrap(),
+        };
+
         let result = estimate_message_fee(rpc, input).await.expect("result");
-        assert_eq!(
-            result,
-            FeeEstimate {
-                gas_consumed: H256::from_str(
-                    "0x000000000000000000000000000000000000000000000000000000000000479a"
-                )
-                .unwrap(),
-                gas_price: H256::from_str(
-                    "0x0000000000000000000000000000000000000000000000000000000000000001"
-                )
-                .unwrap(),
-                overall_fee: H256::from_str(
-                    "0x000000000000000000000000000000000000000000000000000000000000479a"
-                )
-                .unwrap(),
-            }
-        );
+        assert_eq!(result, expected);
+    }
+
+    #[tokio::test]
+    async fn test_error_missing_contract() {
+        let dir = tempdir().expect("tempdir");
+        let mut db_path = dir.path().to_path_buf();
+        db_path.push("db.sqlite");
+
+        let storage = Storage::migrate(db_path, JournalMode::WAL).expect("storage");
+
+        {
+            let mut db = storage.connection().expect("db connection");
+            let tx = db.transaction().expect("tx");
+
+            let hash = ClassHash(felt!(
+                "0x0484c163658bcce5f9916f486171ac60143a92897533aa7ff7ac800b16c63311"
+            ));
+            tx.insert_cairo_class(hash, CAIRO_0_11_WITH_DECIMAL_ENTRY_POINT_OFFSET)
+                .expect("insert class");
+
+            let header = BlockHeader::builder()
+                .with_number(BlockNumber::GENESIS + 1)
+                .with_timestamp(BlockTimestamp::new_or_panic(1))
+                .with_gas_price(GasPrice(1))
+                .finalize_with_hash(BlockHash::ZERO);
+
+            tx.insert_block_header(&header).unwrap();
+
+            // NOTE: contract deployment is missing
+
+            tx.commit().unwrap();
+        }
+
+        let (call_handle, _join_handle) = crate::cairo::ext_py::start(
+            storage.path().into(),
+            std::num::NonZeroUsize::try_from(1).unwrap(),
+            futures::future::pending(),
+            Chain::Testnet,
+        )
+        .await
+        .unwrap();
+
+        let rpc = RpcContext::for_tests()
+            .with_storage(storage)
+            .with_call_handling(call_handle);
+
+        let input = EstimateMessageFeeInput {
+            message: FunctionCall {
+                contract_address: ContractAddress::new_or_panic(felt!(
+                    "0x57dde83c18c0efe7123c36a52d704cf27d5c38cdf0b1e1edc3b0dae3ee4e374"
+                )),
+                entry_point_selector: EntryPoint(felt!(
+                    "0xc73f681176fc7b3f9693986fd7b14581e8d540519e27400e88b8713932be01"
+                )),
+                calldata: vec![CallParam(felt!("0x1")), CallParam(felt!("0x2"))],
+            },
+            sender_address: EthereumAddress(H160::zero()),
+            block_id: BlockId::Number(BlockNumber::new_or_panic(1)),
+        };
+
+        assert_matches::assert_matches!(estimate_message_fee(rpc, input).await, Err(EstimateMessageFeeError::ContractNotFound));
+    }
+
+    #[tokio::test]
+    async fn test_error_missing_block() {
+        let dir = tempdir().expect("tempdir");
+        let mut db_path = dir.path().to_path_buf();
+        db_path.push("db.sqlite");
+
+        let storage = Storage::migrate(db_path, JournalMode::WAL).expect("storage");
+
+        // NOTE: block insertion is missing
+
+        let (call_handle, _join_handle) = crate::cairo::ext_py::start(
+            storage.path().into(),
+            std::num::NonZeroUsize::try_from(1).unwrap(),
+            futures::future::pending(),
+            Chain::Testnet,
+        )
+        .await
+        .unwrap();
+
+        let rpc = RpcContext::for_tests()
+            .with_storage(storage)
+            .with_call_handling(call_handle);
+
+        let input = EstimateMessageFeeInput {
+            message: FunctionCall {
+                contract_address: ContractAddress::new_or_panic(felt!(
+                    "0x57dde83c18c0efe7123c36a52d704cf27d5c38cdf0b1e1edc3b0dae3ee4e374"
+                )),
+                entry_point_selector: EntryPoint(felt!(
+                    "0xc73f681176fc7b3f9693986fd7b14581e8d540519e27400e88b8713932be01"
+                )),
+                calldata: vec![CallParam(felt!("0x1")), CallParam(felt!("0x2"))],
+            },
+            sender_address: EthereumAddress(H160::zero()),
+            block_id: BlockId::Number(BlockNumber::new_or_panic(1)),
+        };
+
+        assert_matches::assert_matches!(estimate_message_fee(rpc, input).await, Err(EstimateMessageFeeError::BlockNotFound));
+    }
+
+
+    #[tokio::test]
+    async fn test_error_invalid_selector() {
+        let dir = tempdir().expect("tempdir");
+        let mut db_path = dir.path().to_path_buf();
+        db_path.push("db.sqlite");
+
+        let storage = Storage::migrate(db_path, JournalMode::WAL).expect("storage");
+
+        {
+            let mut db = storage.connection().expect("db connection");
+            let tx = db.transaction().expect("tx");
+
+            let hash = ClassHash(felt!(
+                "0x0484c163658bcce5f9916f486171ac60143a92897533aa7ff7ac800b16c63311"
+            ));
+            tx.insert_cairo_class(hash, CAIRO_0_11_WITH_DECIMAL_ENTRY_POINT_OFFSET)
+                .expect("insert class");
+
+            let header = BlockHeader::builder()
+                .with_number(BlockNumber::GENESIS + 1)
+                .with_timestamp(BlockTimestamp::new_or_panic(1))
+                .with_gas_price(GasPrice(1))
+                .finalize_with_hash(BlockHash::ZERO);
+
+            tx.insert_block_header(&header).unwrap();
+
+            let addr = ContractAddress::new_or_panic(felt!(
+                "0x57dde83c18c0efe7123c36a52d704cf27d5c38cdf0b1e1edc3b0dae3ee4e374"
+            ));
+            tx.insert_contract_class_hash(BlockNumber::GENESIS + 1, addr, hash)
+                .unwrap();
+
+            tx.commit().unwrap();
+        }
+
+        let (call_handle, _join_handle) = crate::cairo::ext_py::start(
+            storage.path().into(),
+            std::num::NonZeroUsize::try_from(1).unwrap(),
+            futures::future::pending(),
+            Chain::Testnet,
+        )
+        .await
+        .unwrap();
+
+        let rpc = RpcContext::for_tests()
+            .with_storage(storage)
+            .with_call_handling(call_handle);
+
+        let input = EstimateMessageFeeInput {
+            message: FunctionCall {
+                contract_address: ContractAddress::new_or_panic(felt!(
+                    "0x57dde83c18c0efe7123c36a52d704cf27d5c38cdf0b1e1edc3b0dae3ee4e374"
+                )),
+                entry_point_selector: EntryPoint(felt!(
+                    "0xDEADBEEF" // NOTE: invalid selector
+                )),
+                calldata: vec![CallParam(felt!("0x1")), CallParam(felt!("0x2"))],
+            },
+            sender_address: EthereumAddress(H160::zero()),
+            block_id: BlockId::Number(BlockNumber::new_or_panic(1)),
+        };
+
+        assert_matches::assert_matches!(estimate_message_fee(rpc, input).await, Err(EstimateMessageFeeError::ContractError));
     }
 }
