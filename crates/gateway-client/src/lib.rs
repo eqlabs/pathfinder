29fc1a382fcfbf5cd8eef510f078b871dd494703
Sergey Melnychuk <8093171+sergey-melnychuk@users.noreply.github.com>
2023-07-07 15:08:44 +0200

    chore: fmt again

--- crates/gateway-client/src/lib.rs
+++ crates/gateway-client/src/lib.rs
@@ -82,7 +82,11 @@ pub trait GatewayApi: Sync {
 
 #[async_trait::async_trait]
 impl<T: GatewayApi + Sync + Send> GatewayApi for std::sync::Arc<T> {
-    async fn block(&self, block_id: BlockId, is_retry_enabled: bool) -> Result<reply::MaybePendingBlock, SequencerError> {
+    async fn block(
+        &self,
+        block_id: BlockId,
+        is_retry_enabled: bool,
+    ) -> Result<reply::MaybePendingBlock, SequencerError> {
         self.as_ref().block(block_id, is_retry_enabled).await
     }
 
