5ad83f10b780d59d2893e61822cc7bae36de4593
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-07-24 15:07:16 +0200

    chore: improve migration progress logs
    Co-authored-by: Krisztian Kovacs <krisztian@equilibrium.co>

--- crates/storage/src/schema/revision_0038.rs
+++ crates/storage/src/schema/revision_0038.rs
@@ -2,6 +2,7 @@ use anyhow::Context;
 
 /// This migration removes the event key FTS5 table for v0.2 RPC.
 pub(crate) fn migrate(tx: &rusqlite::Transaction<'_>) -> anyhow::Result<()> {
+    tracing::info!("Removing event event data for RPC v0.2, this may take some time.");
     tx.execute_batch(
         "
 DROP TRIGGER starknet_events_ad;
