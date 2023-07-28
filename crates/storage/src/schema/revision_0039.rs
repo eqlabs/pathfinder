8ae93147f68aacf849bcf0c1906019262c7ebf79
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-07-28 12:35:04 +0200

    feat(storage): add missing header columns

--- crates/storage/src/schema/revision_0039.rs
+++ crates/storage/src/schema/revision_0039.rs
@@ -12,5 +12,23 @@ pub(crate) fn migrate(tx: &rusqlite::Transaction<'_>) -> anyhow::Result<()> {
     )
     .context("Renaming headers.root table to headers.storage_commitment")?;
 
+    tx.execute(
+        "ALTER TABLE headers ADD COLUMN state_commitment BLOB NOT NULL DEFAULT x'0000000000000000000000000000000000000000000000000000000000000000'",
+        [],
+    )
+    .context("Adding state_commitment column")?;
+
+    tx.execute(
+        "ALTER TABLE headers ADD COLUMN transaction_count INTEGER NOT NULL DEFAULT 0",
+        [],
+    )
+    .context("Adding transaction_count column")?;
+
+    tx.execute(
+        "ALTER TABLE headers ADD COLUMN event_count INTEGER NOT NULL DEFAULT 0",
+        [],
+    )
+    .context("Adding event_count column")?;
+
     Ok(())
 }
