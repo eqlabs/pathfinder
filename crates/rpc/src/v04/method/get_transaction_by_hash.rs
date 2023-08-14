c93996d07648f52255646d198f92819ae5d8ed2b
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-08-14 11:58:36 +0200

    refactor: factor out get transaction impl

--- crates/rpc/src/v04/method/get_transaction_by_hash.rs
+++ crates/rpc/src/v04/method/get_transaction_by_hash.rs
@@ -2,7 +2,6 @@ use crate::context::RpcContext;
 use crate::v02::types::reply::Transaction;
 
 use crate::v02::method::get_transaction_by_hash as v02_get_transaction_by_hash;
-use v02_get_transaction_by_hash::GetTransactionByHashError as V02_Error;
 
 crate::error::generate_rpc_error_subset!(GetTransactionByHashError: TxnHashNotFoundV04);
 
@@ -11,10 +10,8 @@ pub async fn get_transaction_by_hash(
     input: v02_get_transaction_by_hash::GetTransactionByHashInput,
 ) -> Result<Transaction, GetTransactionByHashError> {
     // v0.4 is a complete copy except we use a different error code.
-    v02_get_transaction_by_hash::get_transaction_by_hash(context, input)
-        .await
-        .map_err(|e| match e {
-            V02_Error::Internal(x) => GetTransactionByHashError::Internal(x),
-            V02_Error::TxnHashNotFoundV03 => GetTransactionByHashError::TxnHashNotFoundV04,
-        })
+    v02_get_transaction_by_hash::get_transaction_by_hash_impl(context, input)
+        .await?
+        .map(Into::into)
+        .ok_or(GetTransactionByHashError::TxnHashNotFoundV04)
 }
