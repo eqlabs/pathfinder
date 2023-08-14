536417fedcfce048c0ffd15b36855ff889644a40
Krzysztof Lis <klis33@gmail.com>
2023-08-14 18:30:02 +0200

    feat(p2p_proto): block

---
+++ crates/p2p_proto/src/lib.rs
@@ -0,0 +1,116 @@
+pub mod proto {
+    pub mod block {
+        include!(concat!(env!("OUT_DIR"), "/starknet.block.rs"));
+    }
+    pub mod common {
+        include!(concat!(env!("OUT_DIR"), "/starknet.common.rs"));
+    }
+}
+
+pub trait ToProtobuf<Output>
+where
+    Self: Sized,
+{
+    fn to_protobuf(self) -> Output;
+}
+
+impl ToProtobuf<u64> for u64 {
+    fn to_protobuf(self) -> u64 {
+        self
+    }
+}
+
+impl ToProtobuf<u32> for u32 {
+    fn to_protobuf(self) -> u32 {
+        self
+    }
+}
+
+impl ToProtobuf<u8> for u8 {
+    fn to_protobuf(self) -> u8 {
+        self
+    }
+}
+
+impl ToProtobuf<String> for String {
+    fn to_protobuf(self) -> String {
+        self
+    }
+}
+
+impl<M, T: ToProtobuf<M>> ToProtobuf<Vec<M>> for Vec<T> {
+    fn to_protobuf(self) -> Vec<M> {
+        self.into_iter().map(ToProtobuf::to_protobuf).collect()
+    }
+}
+
+impl<M, T: ToProtobuf<M>> ToProtobuf<Option<M>> for Option<T> {
+    fn to_protobuf(self) -> Option<M> {
+        self.map(ToProtobuf::to_protobuf)
+    }
+}
+
+pub trait TryFromProtobuf<M>
+where
+    Self: Sized,
+{
+    fn try_from_protobuf(input: M, field_name: &'static str) -> Result<Self, std::io::Error>;
+}
+
+impl TryFromProtobuf<u64> for u64 {
+    fn try_from_protobuf(input: u64, _field_name: &'static str) -> Result<Self, std::io::Error> {
+        Ok(input)
+    }
+}
+
+impl TryFromProtobuf<u32> for u32 {
+    fn try_from_protobuf(input: u32, _field_name: &'static str) -> Result<Self, std::io::Error> {
+        Ok(input)
+    }
+}
+
+impl TryFromProtobuf<u8> for u8 {
+    fn try_from_protobuf(input: u8, _field_name: &'static str) -> Result<Self, std::io::Error> {
+        Ok(input)
+    }
+}
+
+impl TryFromProtobuf<String> for String {
+    fn try_from_protobuf(input: String, _field_name: &'static str) -> Result<Self, std::io::Error> {
+        Ok(input)
+    }
+}
+
+impl<T: TryFromProtobuf<U>, U> TryFromProtobuf<Option<U>> for T {
+    fn try_from_protobuf(
+        input: Option<U>,
+        field_name: &'static str,
+    ) -> Result<Self, std::io::Error> {
+        let input = input.ok_or_else(|| {
+            std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Missing field {field_name}"),
+            )
+        })?;
+        TryFromProtobuf::try_from_protobuf(input, field_name)
+    }
+}
+
+impl<T: TryFromProtobuf<U>, U> TryFromProtobuf<Vec<U>> for Vec<T> {
+    fn try_from_protobuf(input: Vec<U>, field_name: &'static str) -> Result<Self, std::io::Error> {
+        input
+            .into_iter()
+            .map(|e| TryFromProtobuf::try_from_protobuf(e, field_name))
+            .collect::<Result<Vec<_>, _>>()
+            .map_err(|e| {
+                std::io::Error::new(
+                    std::io::ErrorKind::InvalidData,
+                    format!("Failed to parse {field_name}: {e}"),
+                )
+            })
+    }
+}
+
+use p2p_proto_derive::*;
+pub mod block;
+pub mod common;
