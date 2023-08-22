0d6e41c87f697bde7bd6601fd01f33f9ecc39fd1
Krzysztof Lis <klis33@gmail.com>
2023-08-22 14:49:50 +0200

    feat(p2p_network): get_blocks continued

--- crates/pathfinder/src/p2p_network/sync_handlers.rs
+++ crates/pathfinder/src/p2p_network/sync_handlers.rs
@@ -1,6 +1,7 @@
 //! Sync related data retrieval from storage as requested by other p2p clients
 
 use anyhow::Context;
+use pathfinder_common::BlockNumber;
 use pathfinder_storage::{Storage, Transaction};
 
 pub mod v0;
@@ -15,11 +16,36 @@ pub enum GetBlocksReply {
 type GetBlocksReplyTx = tokio::sync::mpsc::Sender<GetBlocksReply>;
 type GetBlocksReplyRx = tokio::sync::mpsc::Receiver<GetBlocksReply>;
 
+/// Returns next block number considering direction.
+///
+/// None is returned if we're out-of-bounds.
+fn get_next_block_number(
+    current: BlockNumber,
+    direction: p2p_proto::block::Direction,
+) -> Option<BlockNumber> {
+    use p2p_proto::block::Direction::{Backward, Forward};
+    match direction {
+        Forward => current.get().checked_add(1).and_then(BlockNumber::new),
+        Backward => current.get().checked_sub(1).and_then(BlockNumber::new),
+    }
+}
+
 pub async fn get_blocks(
     request: p2p_proto::block::GetBlocks,
     storage: &Storage,
     reply_tx: GetBlocksReplyTx,
 ) -> anyhow::Result<()> {
+    let p2p_proto::block::GetBlocks {
+        start,
+        direction,
+        limit,
+        skip,
+        step, // TODO implement step
+    } = request;
+
+    let _step = step; // TODO how does step work...?
+    let limit = limit.min(MAX_BLOCKS_COUNT);
+
     // TODO check if there are faster ways to do this
     // for example retrieve data in batches instead of one by one
     todo!("retrieve one item, push into channel, repeat; max msg limit is maintained")
