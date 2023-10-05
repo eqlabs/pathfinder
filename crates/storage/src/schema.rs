24e4d4d5009f5002e554dd440d3adfdfc00e8063
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-10-05 09:30:31 +0200

    fix: old databases can still migrate
    Requires a no-op revision to create a schism.

--- crates/storage/src/schema.rs
+++ crates/storage/src/schema.rs
@@ -1,6 +1,6 @@
 mod base;
 
-mod revision_0040;
+mod revision_0041;
 
 pub(crate) use base::base_schema;
 
@@ -8,9 +8,10 @@ type MigrationFn = fn(&rusqlite::Transaction<'_>) -> anyhow::Result<()>;
 
 /// The full list of pathfinder migrations.
 pub fn migrations() -> &'static [MigrationFn] {
-    // Don't forget to update `call.py` database version number!
-    &[revision_0040::migrate]
+    &[revision_0041::migrate]
 }
 
 /// The number of schema revisions replaced by the [base schema](base::base_schema).
-pub(crate) const BASE_SCHEMA_REVISION: usize = 39;
+///
+/// Note that 40 was a no-op as we wanted to disallow versions <= 39.
+pub(crate) const BASE_SCHEMA_REVISION: usize = 40;
