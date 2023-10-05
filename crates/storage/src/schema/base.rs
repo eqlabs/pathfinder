b6ef8991940f6c46e3dd12e5cca31e5e4348623a
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-10-05 10:59:37 +0200

    fix: FTS5 table already creates auxilliary tables

--- crates/storage/src/schema/base.rs
+++ crates/storage/src/schema/base.rs
@@ -66,10 +66,6 @@ CREATE VIRTUAL TABLE starknet_events_keys_03 USING fts5(
     content='',
     tokenize='ascii'
 );
-CREATE TABLE starknet_events_keys_03_data(id INTEGER PRIMARY KEY, block BLOB);
-CREATE TABLE starknet_events_keys_03_idx(segid, term, pgno, PRIMARY KEY(segid, term)) WITHOUT ROWID;
-CREATE TABLE starknet_events_keys_03_docsize(id INTEGER PRIMARY KEY, sz BLOB);
-CREATE TABLE starknet_events_keys_03_config(k PRIMARY KEY, v) WITHOUT ROWID;
 CREATE TRIGGER starknet_events_03_ai AFTER INSERT ON starknet_events BEGIN
     INSERT INTO starknet_events_keys_03(rowid, keys) VALUES (
         new.id,
