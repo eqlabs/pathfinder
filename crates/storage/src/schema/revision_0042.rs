054dca9c043ccf63bcd221cccacd8138b99db685
Krisztian Kovacs <krisztian@equilibrium.co>
2023-10-11 16:58:59 +0200

    fix(storage/revision_0042): add missing index for contract_roots
    When purging contract roots we're deleting using just the block number,
    which makes our existing composite index useless.
    
    This leads to falling back to doing table scans during block purges.
    
    This PR adds a migration that creates the missing index on the block_number
    column.

---
+++ crates/storage/src/schema/revision_0042.rs
@@ -0,0 +1,11 @@
+use anyhow::Context;
+
+pub(crate) fn migrate(tx: &rusqlite::Transaction<'_>) -> anyhow::Result<()> {
+    tx.execute(
+        "CREATE INDEX contract_roots_block_number ON contract_roots(block_number)",
+        [],
+    )
+    .context("Creating index on contract_roots(block_number)")?;
+
+    Ok(())
+}
