abd7dd87f7694463a7c0430086494e5ff1e105c0
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2023-11-09 10:02:51 +0200

    feat(rpc): `starknet_specVersion` bumped to v0.5.1
    Also simplified it by changing to a closure since its just returning
    a static str.

--- crates/rpc/src/v05/method.rs
+++ crates/rpc/src/v05/method.rs
@@ -6,7 +6,6 @@ mod get_block_with_txs;
 mod get_transaction_receipt;
 mod get_transaction_status;
 mod simulate_transactions;
-mod spec_version;
 mod trace_block_transactions;
 mod trace_transaction;
 
@@ -18,6 +17,5 @@ pub(crate) use get_block_with_txs::get_block_with_txs;
 pub(crate) use get_transaction_receipt::get_transaction_receipt;
 pub(crate) use get_transaction_status::get_transaction_status;
 pub(crate) use simulate_transactions::simulate_transactions;
-pub(crate) use spec_version::spec_version;
 pub(crate) use trace_block_transactions::trace_block_transactions;
 pub(crate) use trace_transaction::trace_transaction;
