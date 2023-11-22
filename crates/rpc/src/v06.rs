a25a4ea0ed98e2e7bf54ead9158d4df9a269fa5a
Krisztian Kovacs <krisztian@equilibrium.co>
2023-11-22 15:58:34 +0100

    feat(rpc/v06): add `starknet_estimateFee`
    Same as the v05 implementation.

--- crates/rpc/src/v06.rs
+++ crates/rpc/src/v06.rs
@@ -29,13 +29,13 @@ pub fn register_routes() -> RpcRouterBuilder {
         .register("starknet_syncing"                         , v04_method::syncing)
 
         .register("starknet_call"                            , v05_method::call)
+        .register("starknet_estimateFee"                     , v05_method::estimate_fee)
 
         .register("starknet_getBlockWithTxs"                 , method::get_block_with_txs)
 
         // .register("starknet_addDeclareTransaction"           , method::add_declare_transaction)
         // .register("starknet_addDeployAccountTransaction"     , method::add_deploy_account_transaction)
         // .register("starknet_addInvokeTransaction"            , method::add_invoke_transaction)
-        // .register("starknet_estimateFee"                     , method::estimate_fee)
         // .register("starknet_estimateMessageFee"              , method::estimate_message_fee)
         // .register("starknet_getBlockWithTxHashes"            , method::get_block_with_tx_hashes)
         // .register("starknet_getTransactionStatus"            , method::get_transaction_status)
