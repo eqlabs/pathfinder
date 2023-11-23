81f03e90df0394c1d5e2724b80bf8a59dc858d58
Krisztian Kovacs <krisztian@equilibrium.co>
2023-11-23 14:52:45 +0100

    feat(rpc/v06): add `starknet_traceBlockTransactions` and `starknet_traceTransaction`
    These are really just copies of the v05 implementation but using the
    `TransansactionTrace` type for v06.

--- crates/rpc/src/v06.rs
+++ crates/rpc/src/v06.rs
@@ -35,16 +35,15 @@ pub fn register_routes() -> RpcRouterBuilder {
 
         .register("starknet_getBlockWithTxHashes"            , method::get_block_with_tx_hashes)
         .register("starknet_getBlockWithTxs"                 , method::get_block_with_txs)
-        .register("starknet_simulateTransactions"            , method::simulate_transactions)
         .register("starknet_getTransactionReceipt"           , method::get_transaction_receipt)
+        .register("starknet_simulateTransactions"            , method::simulate_transactions)
+        .register("starknet_traceBlockTransactions"          , method::trace_block_transactions)
+        .register("starknet_traceTransaction"                , method::trace_transaction)
 
         // .register("starknet_addDeclareTransaction"           , method::add_declare_transaction)
         // .register("starknet_addDeployAccountTransaction"     , method::add_deploy_account_transaction)
         // .register("starknet_addInvokeTransaction"            , method::add_invoke_transaction)
-        // .register("starknet_getTransactionStatus"            , method::get_transaction_status)
         .register("starknet_specVersion"                     , || "0.6.0-rc1")
-        // .register("starknet_traceBlockTransactions"          , method::trace_block_transactions)
-        // .register("starknet_traceTransaction"                , method::trace_transaction)
 
         .register("pathfinder_getProof"                      , crate::pathfinder::methods::get_proof)
 }
--- crates/rpc/src/v06/method.rs
+++ crates/rpc/src/v06/method.rs
@@ -2,8 +2,12 @@ mod get_block_with_tx_hashes;
 mod get_block_with_txs;
 pub(crate) mod get_transaction_receipt;
 mod simulate_transactions;
+mod trace_block_transactions;
+mod trace_transaction;
 
 pub(crate) use get_block_with_tx_hashes::get_block_with_tx_hashes;
 pub(crate) use get_block_with_txs::get_block_with_txs;
 pub(super) use get_transaction_receipt::get_transaction_receipt;
 pub(crate) use simulate_transactions::simulate_transactions;
+pub(crate) use trace_block_transactions::trace_block_transactions;
+pub(crate) use trace_transaction::trace_transaction;
