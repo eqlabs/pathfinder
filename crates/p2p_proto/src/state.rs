6c4a3ead9accc2510165f15c35824655c63e4656
Krzysztof Lis <klis33@gmail.com>
2023-12-11 15:52:41 +0100

    feat(p2p_proto): update class messages

--- crates/p2p_proto/src/state.rs
+++ crates/p2p_proto/src/state.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-use crate::common::{Address, Hash};
+use crate::common::Address;
 use crate::{ToProtobuf, TryFromProtobuf};
 use fake::Dummy;
 use pathfinder_crypto::Felt;
@@ -30,28 +30,85 @@ pub struct StateDiff {
     pub contract_diffs: Vec<ContractDiff>,
 }
 
-#[derive(Clone, PartialEq, Eq, ToProtobuf, TryFromProtobuf, Dummy)]
-#[protobuf(name = "crate::proto::state::Class")]
-pub struct Class {
-    pub compiled_hash: Hash,
-    pub definition: Vec<u8>,
-    #[optional]
-    pub total_parts: Option<u32>,
-    #[optional]
-    pub part_num: Option<u32>,
-    // FIXME needed for sierra classes, occurs in first chunk
-    #[optional]
-    pub casm_hash: Option<Hash>,
+#[derive(Debug, Clone, PartialEq, Eq, ToProtobuf, TryFromProtobuf, Dummy)]
+#[protobuf(name = "crate::proto::state::EntryPoint")]
+pub struct EntryPoint {
+    pub selector: Felt,
+    pub offset: Felt,
 }
 
-impl Debug for Class {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Class")
-            .field("compiled_hash", &self.compiled_hash)
-            .field("definition.len", &self.definition.len())
-            .field("total_parts", &self.total_parts)
-            .field("part_num", &self.part_num)
-            .finish()
+#[derive(Debug, Clone, PartialEq, Eq, ToProtobuf, TryFromProtobuf, Dummy)]
+#[protobuf(name = "crate::proto::state::Cairo0Class")]
+pub struct Cairo0Class {
+    pub abi: Vec<u8>,
+    pub externals: Vec<EntryPoint>,
+    pub l1_handlers: Vec<EntryPoint>,
+    pub constructors: Vec<EntryPoint>,
+    pub program: Vec<u8>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, ToProtobuf, TryFromProtobuf, Dummy)]
+#[protobuf(name = "crate::proto::state::SierraEntryPoint")]
+pub struct SierraEntryPoint {
+    pub index: u64,
+    pub selector: Felt,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, ToProtobuf, TryFromProtobuf, Dummy)]
+#[protobuf(name = "crate::proto::state::Cairo1EntryPoints")]
+pub struct Cairo1EntryPoints {
+    pub externals: Vec<SierraEntryPoint>,
+    pub l1_handlers: Vec<SierraEntryPoint>,
+    pub constructors: Vec<SierraEntryPoint>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, ToProtobuf, TryFromProtobuf, Dummy)]
+#[protobuf(name = "crate::proto::state::Cairo1Class")]
+pub struct Cairo1Class {
+    pub abi: Vec<u8>,
+    pub entry_points: Cairo1EntryPoints,
+    pub program: Vec<Felt>,
+    pub contract_class_version: String,
+    pub compiled: Vec<u8>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Dummy)]
+pub enum Class {
+    Cairo0(Cairo0Class),
+    Cairo1(Cairo1Class),
+}
+
+impl ToProtobuf<crate::proto::state::Class> for Class {
+    fn to_protobuf(self) -> crate::proto::state::Class {
+        use crate::proto::state::{
+            class::Class::{Cairo0, Cairo1},
+            Class,
+        };
+        match self {
+            Self::Cairo0(c) => Class {
+                class: Some(Cairo0(c.to_protobuf())),
+            },
+            Self::Cairo1(c) => Class {
+                class: Some(Cairo1(c.to_protobuf())),
+            },
+        }
+    }
+}
+
+impl TryFromProtobuf<crate::proto::state::Class> for Class {
+    fn try_from_protobuf(
+        input: crate::proto::state::Class,
+        field_name: &'static str,
+    ) -> Result<Self, std::io::Error> {
+        use crate::proto::state::class::Class::{Cairo0, Cairo1};
+        match input.class {
+            Some(Cairo0(c)) => Ok(Self::Cairo0(Cairo0Class::try_from_protobuf(c, field_name)?)),
+            Some(Cairo1(c)) => Ok(Self::Cairo1(Cairo1Class::try_from_protobuf(c, field_name)?)),
+            None => Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("{}: class field missing", field_name),
+            )),
+        }
     }
 }
 
