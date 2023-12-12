b2a811b0bbe2add0446ab234b69604c9c74de424
Krzysztof Lis <klis33@gmail.com>
2023-12-12 11:04:13 +0100

    test(p2p_proto): remove class encoding overhead check

--- crates/p2p_proto/src/consts.rs
+++ crates/p2p_proto/src/consts.rs
@@ -1,29 +1,21 @@
 /// Constants that allow us to estimate the maximum payload of certain types of messages
 /// Maximum size of an encoded protobuf message in bytes
 pub const MESSAGE_SIZE_LIMIT: usize = 20 * 1024 * 1024;
-pub const CLASSES_MESSAGE_OVERHEAD: usize = 58;
-/// Upper bound
-pub const PER_CLASS_OVERHEAD: usize = 96;
 /// Upper bound
 pub const ENCODED_HEADER_SIZE: usize = 483;
 pub const HEADERS_MESSAGE_OVERHEAD: usize = 1;
 /// Lower bound
 pub const MAX_HEADERS_PER_MESSAGE: usize =
     (MESSAGE_SIZE_LIMIT - HEADERS_MESSAGE_OVERHEAD + ENCODED_HEADER_SIZE) / ENCODED_HEADER_SIZE;
-/// A wild guess at an upper bound to allow discarding classes that couldn't have been created by the cairo compiler
-/// so must come from a faulty/malicious agent.
-pub const MAX_PARTS_PER_CLASS: u32 = 10;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::proto::block::{block_bodies_response::BodyMessage, BlockBodiesResponse};
     use crate::proto::block::{
         block_headers_response_part::HeaderMessage, BlockHeader, BlockHeadersResponse,
         BlockHeadersResponsePart,
     };
-    use crate::proto::common::{Address, BlockId, Hash, Merkle, Patricia};
-    use crate::proto::state::{Class, Classes};
+    use crate::proto::common::{Address, Hash, Merkle, Patricia};
     use prost::Message;
     use prost_types::Timestamp;
 
@@ -61,35 +53,6 @@ mod tests {
         }
     }
 
-    #[cfg(test)]
-    #[test]
-    fn check_classes_message_overhead_upper_bound() {
-        let response = |classes| BlockBodiesResponse {
-            id: Some(BlockId {
-                header: Some(Hash::full()),
-                number: u64::MAX,
-            }),
-            body_message: Some(BodyMessage::Classes(Classes {
-                domain: u32::MAX,
-                classes,
-            })),
-        };
-        let class = Class {
-            compiled_hash: Some(Hash::full()),
-            definition: vec![0xFF; MESSAGE_SIZE_LIMIT],
-            total_parts: Some(u32::MAX),
-            part_num: Some(u32::MAX),
-            casm_hash: Some(Hash::full()),
-        };
-        let len = response(vec![class]).encode_length_delimited_to_vec().len();
-        assert_eq!(
-            len,
-            CLASSES_MESSAGE_OVERHEAD + (PER_CLASS_OVERHEAD + MESSAGE_SIZE_LIMIT)
-        );
-    }
-
-    #[cfg(test)]
-    #[rstest::rstest]
     #[test]
     fn check_headers_message_size_upper_bound() {
         let part = BlockHeadersResponsePart {
