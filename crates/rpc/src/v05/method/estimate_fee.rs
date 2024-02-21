40674ee0250dea703ada7dc5e7241b2566add3ae
Krisztian Kovacs <krisztian@equilibrium.co>
2024-02-21 14:31:48 +0100

    Revert "fix v05 tests broken by blockifier update"
    This reverts commit 5d194b5a3f59c7ceef6fbc4bc6ac812e45e5e5fb.

--- crates/rpc/src/v05/method/estimate_fee.rs
+++ crates/rpc/src/v05/method/estimate_fee.rs
@@ -348,26 +348,26 @@ pub(crate) mod tests {
             };
             let result = estimate_fee(context, input).await.unwrap();
             let declare_expected = FeeEstimate {
-                gas_consumed: 26571.into(),
+                gas_consumed: 2768.into(),
                 gas_price: 1.into(),
-                overall_fee: 26571.into(),
+                overall_fee: 2768.into(),
             };
             let deploy_expected = FeeEstimate {
-                gas_consumed: 3008.into(),
+                gas_consumed: 3020.into(),
                 gas_price: 1.into(),
-                overall_fee: 3008.into(),
+                overall_fee: 3020.into(),
             };
             let invoke_expected = FeeEstimate {
-                gas_consumed: 1664.into(),
+                gas_consumed: 1674.into(),
                 gas_price: 1.into(),
-                overall_fee: 1664.into(),
+                overall_fee: 1674.into(),
             };
             let invoke_v0_expected = FeeEstimate {
-                gas_consumed: 872.into(),
+                gas_consumed: 880.into(),
                 gas_price: 1.into(),
-                overall_fee: 872.into(),
+                overall_fee: 880.into(),
             };
-            pretty_assertions_sorted::assert_eq!(
+            assert_eq!(
                 result,
                 vec![
                     declare_expected,
