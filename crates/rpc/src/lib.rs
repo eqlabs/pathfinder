6219cc8cab15162bcc98efa3769f21d4e4e7f21e
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-02-23 11:30:54 +0200

    feat(rpc/v0.7): starknet_getBlockWithTxs
    Copied from v0.6 with some DTO related changes.
    
    Co-authored-by: Krisztian Kovacs <krisztian@equilibrium.co>

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -828,7 +828,6 @@ mod tests {
         "starknet_getTransactionStatus",
         "starknet_estimateFee",
         "starknet_estimateMessageFee",
-        "starknet_getBlockWithTxs",
         "starknet_getTransactionByBlockIdAndIndex",
         "starknet_getTransactionByHash",
     ])]
--- crates/rpc/src/v07.rs
+++ crates/rpc/src/v07.rs
@@ -30,7 +30,7 @@ pub fn register_routes() -> RpcRouterBuilder {
         // .register("starknet_estimateFee"                     , method::estimate_fee)
         // .register("starknet_estimateMessageFee"              , method::estimate_message_fee)
         .register("starknet_getBlockWithTxHashes",                method::get_block_with_tx_hashes)
-        // .register("starknet_getBlockWithTxs"                 , method::get_block_with_txs)
+        .register("starknet_getBlockWithTxs",                     method::get_block_with_txs)
         // .register("starknet_getTransactionByBlockIdAndIndex" , method::get_transaction_by_block_id_and_index)
         // .register("starknet_getTransactionByHash"            , method::get_transaction_by_hash)
         .register("starknet_getTransactionReceipt",               method::get_transaction_receipt)
