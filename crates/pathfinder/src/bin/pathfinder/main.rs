6b5c62ba08e7ebe073ca2d15c9d5daf2e3e8a8b3
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-02-27 07:56:48 +0200

    Revert "feat(rpc): remove v04 config option, v03 and v04 specs, update Readme"
    This reverts commit d95d2e1720ff1fa41e99a8f0d36cea43e27e209a.

--- crates/pathfinder/src/bin/pathfinder/main.rs
+++ crates/pathfinder/src/bin/pathfinder/main.rs
@@ -197,6 +197,7 @@ Hint: This is usually caused by exceeding the file descriptor limit of your syst
     };
 
     let default_version = match config.rpc_root_version {
+        config::RpcVersion::V04 => pathfinder_rpc::DefaultVersion::V04,
         config::RpcVersion::V05 => pathfinder_rpc::DefaultVersion::V05,
         config::RpcVersion::V06 => pathfinder_rpc::DefaultVersion::V06,
         config::RpcVersion::V07 => pathfinder_rpc::DefaultVersion::V07,
