3add60f4e5c0191c5b0dc87e90e5ee9b10440c05
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-02-29 11:56:40 +0200

    feat(rpc): EVENTS_CHUNK DTO

--- crates/rpc/src/dto/event.rs
+++ crates/rpc/src/dto/event.rs
@@ -12,6 +12,14 @@ pub struct EmittedEvent<'a> {
     pub transaction_hash: &'a pathfinder_common::TransactionHash,
 }
 
+pub struct EventsChunk<'a> {
+    pub events: &'a [pathfinder_common::event::Event],
+    pub continuation_token: Option<&'a str>,
+    pub block_hash: Option<&'a pathfinder_common::BlockHash>,
+    pub block_number: Option<pathfinder_common::BlockNumber>,
+    pub transaction_hash: &'a pathfinder_common::TransactionHash,
+}
+
 impl SerializeForVersion for Event<'_> {
     fn serialize(&self, serializer: Serializer) -> Result<Ok, Error> {
         let mut serializer = serializer.serialize_struct()?;
@@ -55,6 +63,27 @@ impl SerializeForVersion for EmittedEvent<'_> {
         serializer.serialize_field("transaction_hash", &TxnHash(self.transaction_hash))?;
 
         serializer.end()
+    }
+}
+
+impl SerializeForVersion for EventsChunk<'_> {
+    fn serialize(&self, serializer: Serializer) -> Result<Ok, Error> {
+        let mut s = serializer.serialize_struct()?;
+
+        s.serialize_iter(
+            "events",
+            self.events.len(),
+            &mut self.events.iter().map(|x| EmittedEvent {
+                event: x,
+                block_hash: self.block_hash,
+                block_number: self.block_number,
+                transaction_hash: self.transaction_hash,
+            }),
+        )?;
+
+        s.serialize_optional("continuation_token", self.continuation_token)?;
+
+        s.end()
     }
 }
 
@@ -133,6 +162,46 @@ mod tests {
 
         let encoded = EmittedEvent {
             event: &event,
+            block_hash: Some(&block_hash),
+            block_number: Some(block_number),
+            transaction_hash: &tx_hash,
+        }
+        .serialize(s)
+        .unwrap();
+
+        assert_eq!(encoded, expected);
+    }
+
+    #[test]
+    fn events_chunk() {
+        let s = Serializer::default();
+
+        let events = vec![test_event(), test_event()];
+        let tx_hash = transaction_hash!("0x1234");
+        let block_hash = block_hash!("0x222");
+        let block_number = pathfinder_common::BlockNumber::GENESIS + 10;
+        let token = "Hello there";
+
+        let expected = serde_json::json!({
+            "continuation_token": token,
+            "events": events
+                .iter()
+                .map(|x| {
+                    EmittedEvent {
+                        event: x,
+                        block_hash: Some(&block_hash),
+                        block_number: Some(block_number),
+                        transaction_hash: &tx_hash,
+                    }
+                    .serialize(s)
+                    .unwrap()
+                })
+                .collect::<Vec<_>>()
+        });
+
+        let encoded = EventsChunk {
+            events: &events,
+            continuation_token: Some(token),
             block_hash: Some(&block_hash),
             block_number: Some(block_number),
             transaction_hash: &tx_hash,
