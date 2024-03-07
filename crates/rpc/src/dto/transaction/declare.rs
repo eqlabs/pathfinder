bd5802cd1e535c0e9bc99bcdfcac4c6c4d5186b0
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-03-07 14:55:43 +0200

    feat(rpc): DECLARE_TXN_V1 DTO

--- crates/rpc/src/dto/transaction/declare.rs
+++ crates/rpc/src/dto/transaction/declare.rs
@@ -14,6 +14,11 @@ pub(crate) struct DeclareTxnV0<'a> {
     pub(crate) query: bool,
 }
 
+pub(crate) struct DeclareTxnV1<'a> {
+    pub(crate) inner: &'a common::DeclareTransactionV0V1,
+    pub(crate) query: bool,
+}
+
 impl SerializeForVersion for DeclareTxnV0<'_> {
     fn serialize(&self, serializer: Serializer) -> Result<serialize::Ok, serialize::Error> {
         let mut serializer = serializer.serialize_struct()?;
@@ -29,6 +34,28 @@ impl SerializeForVersion for DeclareTxnV0<'_> {
         };
         serializer.serialize_field("version", &version)?;
         serializer.serialize_field("signature", &Signature(&self.inner.signature))?;
+        serializer.serialize_field("class_hash", &Felt(&self.inner.class_hash.0))?;
+
+        serializer.end()
+    }
+}
+
+impl SerializeForVersion for DeclareTxnV1<'_> {
+    fn serialize(&self, serializer: Serializer) -> Result<serialize::Ok, serialize::Error> {
+        let mut serializer = serializer.serialize_struct()?;
+
+        serializer.serialize_field("type", &"DECLARE")?;
+        serializer.serialize_field("sender_address", &Address(&self.inner.sender_address))?;
+        serializer.serialize_field("max_fee", &Felt(&self.inner.max_fee.0))?;
+
+        let version = if self.query {
+            "0x100000000000000000000000000000001"
+        } else {
+            "0x1"
+        };
+        serializer.serialize_field("version", &version)?;
+        serializer.serialize_field("signature", &Signature(&self.inner.signature))?;
+        serializer.serialize_field("nonce", &Felt(&self.inner.nonce.0))?;
         serializer.serialize_field("class_hash", &Felt(&self.inner.class_hash.0))?;
 
         serializer.end()
