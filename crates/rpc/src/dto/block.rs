830972931b14e3397a4e2cd047bb21a7ffbb1ca4
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-03-07 16:11:32 +0200

    feat(rpc): BLOCK_BODY_WITH_RECEIPTS

--- crates/rpc/src/dto/block.rs
+++ crates/rpc/src/dto/block.rs
@@ -4,6 +4,16 @@ use crate::dto::*;
 
 struct BlockBodyWithTxHashes<'a>(&'a [pathfinder_common::TransactionHash]);
 struct BlockBodyWithTxs<'a>(&'a [(&'a pathfinder_common::transaction::Transaction, bool)]);
+struct BlockBodyWithReceipts<'a> {
+    body: &'a [(
+        &'a pathfinder_common::transaction::Transaction,
+        &'a pathfinder_common::receipt::Receipt,
+        bool,
+    )],
+    block_hash: &'a pathfinder_common::BlockHash,
+    block_number: pathfinder_common::BlockNumber,
+    finality: TxnFinalityStatus,
+}
 
 impl SerializeForVersion for BlockBodyWithTxHashes<'_> {
     fn serialize(
@@ -53,6 +63,59 @@ impl SerializeForVersion for BlockBodyWithTxs<'_> {
             &mut self.0.iter().map(|(t, query)| Item {
                 transaction: *t,
                 query: *query,
+            }),
+        )
+    }
+}
+
+impl SerializeForVersion for BlockBodyWithReceipts<'_> {
+    fn serialize(
+        &self,
+        serializer: serialize::Serializer,
+    ) -> Result<serialize::Ok, serialize::Error> {
+        struct Item<'a> {
+            transaction: &'a pathfinder_common::transaction::Transaction,
+            receipt: &'a pathfinder_common::receipt::Receipt,
+            block_hash: &'a pathfinder_common::BlockHash,
+            block_number: pathfinder_common::BlockNumber,
+            finality: TxnFinalityStatus,
+            query: bool,
+        }
+        impl SerializeForVersion for Item<'_> {
+            fn serialize(
+                &self,
+                serializer: serialize::Serializer,
+            ) -> Result<serialize::Ok, serialize::Error> {
+                let mut serializer = serializer.serialize_struct()?;
+
+                serializer.serialize_field(
+                    "receipt",
+                    &TxnReceipt {
+                        receipt: self.receipt,
+                        transaction: self.transaction,
+                        finality: self.finality,
+                        block_hash: self.block_hash,
+                        block_number: self.block_number,
+                    },
+                )?;
+                serializer.flatten(&Txn {
+                    variant: &self.transaction.variant,
+                    query: self.query,
+                })?;
+
+                serializer.end()
+            }
+        }
+
+        serializer.serialize_iter(
+            self.body.len(),
+            &mut self.body.iter().map(|(t, r, query)| Item {
+                transaction: *t,
+                query: *query,
+                receipt: *r,
+                block_hash: self.block_hash,
+                block_number: self.block_number,
+                finality: self.finality,
             }),
         )
     }
