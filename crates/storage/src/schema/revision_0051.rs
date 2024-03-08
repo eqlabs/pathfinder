9fd7d3bcb2398120157651128fb1bbcbbe875e99
Krzysztof Lis <klis33@gmail.com>
2024-03-08 10:49:24 +0100

    fix(storage): migrate past blocks

--- crates/storage/src/schema/revision_0051.rs
+++ crates/storage/src/schema/revision_0051.rs
@@ -1,9 +1,4 @@
-use std::time::{Duration, Instant};
-
 use anyhow::Context;
-use rusqlite::{params, OptionalExtension};
-
-use crate::params::RowExt;
 
 pub(crate) fn migrate(tx: &rusqlite::Transaction<'_>) -> anyhow::Result<()> {
     tracing::info!("Adding new columns to block_headers table");
@@ -13,76 +8,12 @@ pub(crate) fn migrate(tx: &rusqlite::Transaction<'_>) -> anyhow::Result<()> {
         ALTER TABLE block_headers ADD COLUMN nonce_updates_count INTEGER DEFAULT 0;
         ALTER TABLE block_headers ADD COLUMN declared_classes_count INTEGER DEFAULT 0;
         ALTER TABLE block_headers ADD COLUMN deployed_contracts_count INTEGER DEFAULT 0;
-    ",
+        
+        UPDATE block_headers SET
+            storage_diffs_count=(SELECT COUNT(1) FROM storage_updates WHERE block_number=block_headers.number),
+            nonce_updates_count=(SELECT COUNT(1) FROM nonce_updates WHERE block_number=block_headers.number),
+            declared_classes_count=(SELECT COUNT(1) FROM class_definitions WHERE block_number=block_headers.number),
+            deployed_contracts_count=(SELECT COUNT(1) FROM contract_updates WHERE block_number=block_headers.number);",
     )
-    .context("Adding new columns to block_headers")?;
-
-    let mut storage_diffs_query_statement =
-        tx.prepare("SELECT COUNT(*) FROM storage_updates WHERE block_number = ?")?;
-
-    let mut nonce_query_statement =
-        tx.prepare("SELECT COUNT(*) FROM nonce_updates WHERE block_number = ?")?;
-
-    let mut declared_query_statement =
-        tx.prepare("SELECT COUNT(*) FROM class_definitions WHERE block_number = ?")?;
-
-    let mut deployed_query_statement =
-        tx.prepare("SELECT COUNT(*) FROM contract_updates WHERE block_number = ?")?;
-
-    let mut insert_statement = tx.prepare(
-        "INSERT INTO state_update_counts (block_number, storage_diffs, nonce_updates, declared_classes, deployed_contracts) VALUES (?, ?, ?, ?, ?)")?;
-
-    let Some(highest_block) = tx
-        .query_row(
-            "SELECT number FROM canonical_blocks ORDER BY number DESC LIMIT 1",
-            params![],
-            |row| {
-                let number = row.get_block_number(0)?;
-                Ok(number)
-            },
-        )
-        .optional()?
-    else {
-        return Ok(());
-    };
-
-    let mut progress_logged = Instant::now();
-    const LOG_RATE: Duration = Duration::from_secs(10);
-
-    for block_number in 0..=highest_block.get() {
-        if block_number % 1000 == 0 && progress_logged.elapsed() > LOG_RATE {
-            tracing::debug!(%block_number, "Processing events");
-            progress_logged = Instant::now();
-        }
-
-        let storage_diffs = storage_diffs_query_statement
-            .query_row(params![block_number], |row| row.get::<_, i64>(0))
-            .optional()?
-            .unwrap_or(0);
-
-        let nonce_updates = nonce_query_statement
-            .query_row(params![block_number], |row| row.get::<_, i64>(0))
-            .optional()?
-            .unwrap_or(0);
-
-        let declared_classes = declared_query_statement
-            .query_row(params![block_number], |row| row.get::<_, i64>(0))
-            .optional()?
-            .unwrap_or(0);
-
-        let deployed_contracts = deployed_query_statement
-            .query_row(params![block_number], |row| row.get::<_, i64>(0))
-            .optional()?
-            .unwrap_or(0);
-
-        insert_statement.execute(params![
-            &block_number,
-            &storage_diffs,
-            &nonce_updates,
-            &declared_classes,
-            &deployed_contracts
-        ])?;
-    }
-
-    Ok(())
+    .context("Adding new columns to block_headers")
 }
