8708de3723fd81baa0735f73afa915ebfc37e14f
Krisztian Kovacs <krisztian@equilibrium.co>
2024-04-29 09:11:20 +0200

    feat(common): add block commitment signature verification

--- crates/crypto/src/signature/mod.rs
+++ crates/crypto/src/signature/mod.rs
@@ -1,3 +1,10 @@
 mod ecdsa;
 
-pub use ecdsa::{ecdsa_sign, ecdsa_sign_k, ecdsa_verify, ecdsa_verify_partial, get_pk};
+pub use ecdsa::{
+    ecdsa_sign,
+    ecdsa_sign_k,
+    ecdsa_verify,
+    ecdsa_verify_partial,
+    get_pk,
+    SignatureError,
+};
