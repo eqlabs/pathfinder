c3966c7157335b91e0cff862a640289fc432744c
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-05-08 14:26:00 +0200

    feat(rpc): EMITTED_EVENT DTO

--- crates/rpc/src/dto/event.rs
+++ crates/rpc/src/dto/event.rs
@@ -3,7 +3,24 @@ use crate::dto::serialize::{self, SerializeForVersion, Serializer};
 
 pub struct EventsChunk<'a>(pub &'a crate::method::get_events::types::GetEventsResult);
 
+pub struct EmittedEvent<'a>(pub &'a crate::method::get_events::types::EmittedEvent);
+
 impl SerializeForVersion for EventsChunk<'_> {
+    fn serialize(&self, serializer: Serializer) -> Result<serialize::Ok, serialize::Error> {
+        let mut serializer = serializer.serialize_struct()?;
+
+        serializer.serialize_iter(
+            "events",
+            self.0.events.len(),
+            &mut self.0.events.iter().map(EmittedEvent),
+        )?;
+        serializer.serialize_optional("continuation_token", self.0.continuation_token.as_ref())?;
+
+        serializer.end()
+    }
+}
+
+impl SerializeForVersion for EmittedEvent<'_> {
     fn serialize(&self, serializer: Serializer) -> Result<serialize::Ok, serialize::Error> {
         todo!()
     }
