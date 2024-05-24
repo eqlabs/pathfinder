b23aa5af589779e586af0c1096172d4c7c37f014
Mirko von Leipzig <mirko.vonleipzig@gmail.com>
2024-05-24 17:42:28 +0200

    feat(sync): chunk stage

--- crates/pathfinder/src/sync/stream.rs
+++ crates/pathfinder/src/sync/stream.rs
@@ -49,6 +49,54 @@ impl<T: Send + 'static> SyncReceiver<T> {
         SyncReceiver::from_receiver(rx)
     }
 
+    /// Adds a stage which chunks the incoming elements into a vector before
+    /// passing it on.
+    ///
+    /// `capacity` specifies the number of elements, `buffer` specifies the
+    /// output buffering.
+    pub fn try_chunks(mut self, capacity: usize, buffer: usize) -> SyncReceiver<Vec<T>> {
+        let (tx, rx) = tokio::sync::mpsc::channel(buffer);
+
+        std::thread::spawn(move || {
+            let mut chunk = Vec::with_capacity(capacity);
+            let mut peer = PeerId::random();
+
+            while let Some(input) = self.inner.blocking_recv() {
+                // Have a single point of checking this instead of requiring a check at multiple
+                // send points.
+                if tx.is_closed() {
+                    break;
+                }
+
+                let input = match input {
+                    Ok(x) => x,
+                    Err(e) => {
+                        if !chunk.is_empty() {
+                            _ = tx.blocking_send(Ok(PeerData::new(peer, chunk)));
+                        }
+
+                        _ = tx.blocking_send(Err(e));
+                        break;
+                    }
+                };
+
+                // 1st element, assign peer ID.
+                if chunk.is_empty() {
+                    peer = input.peer;
+                }
+
+                chunk.push(input.data);
+
+                if chunk.len() == capacity {
+                    _ = tx.blocking_send(Ok(PeerData::new(peer, chunk)));
+                    chunk = Vec::with_capacity(capacity);
+                }
+            }
+        });
+
+        SyncReceiver::from_receiver(rx)
+    }
+
     pub fn from_receiver(receiver: Receiver<SyncResult<T>>) -> Self
     where
         T: Send,
@@ -81,6 +129,10 @@ impl<T: Send + 'static> SyncReceiver<T> {
         self.inner.recv().await
     }
 }
+
+/// A [ProcessStage] which buffers `N` elements into a vector before passing it
+/// on.
+pub struct Buffer(pub usize);
 
 #[cfg(test)]
 mod tests {
