b9104f387df005abce50f567d616cca5fcd4ec9d
Krisztian Kovacs <krisztian@equilibrium.co>
2024-06-20 12:24:26 +0200

    fix(executor): implement felt conversion for new felt type

--- crates/executor/src/felt.rs
+++ crates/executor/src/felt.rs
@@ -1,37 +1,37 @@
 use pathfinder_crypto::Felt;
-use starknet_api::hash::StarkFelt;
+use starknet_types_core::felt::Felt as CoreFelt;
 
 pub trait IntoFelt {
     fn into_felt(self) -> Felt;
 }
 
-impl IntoFelt for StarkFelt {
+impl IntoFelt for CoreFelt {
     fn into_felt(self) -> Felt {
-        Felt::from_be_slice(self.bytes()).expect("StarkFelt should fit into Felt")
+        Felt::from_be_bytes(self.to_bytes_be()).expect("StarkFelt should fit into Felt")
     }
 }
 
-impl IntoFelt for &StarkFelt {
+impl IntoFelt for &CoreFelt {
     fn into_felt(self) -> Felt {
-        Felt::from_be_slice(self.bytes()).expect("StarkFelt should fit into Felt")
+        Felt::from_be_bytes(self.to_bytes_be()).expect("StarkFelt should fit into Felt")
     }
 }
 
 pub trait IntoStarkFelt {
-    fn into_starkfelt(self) -> StarkFelt;
+    fn into_starkfelt(self) -> CoreFelt;
 }
 
 impl IntoStarkFelt for Felt {
-    fn into_starkfelt(self) -> StarkFelt {
-        StarkFelt::new(self.to_be_bytes()).expect("Felt should fit into StarkFelt")
+    fn into_starkfelt(self) -> CoreFelt {
+        CoreFelt::from_bytes_be(self.as_be_bytes())
     }
 }
 
 impl IntoStarkFelt for pathfinder_common::transaction::DataAvailabilityMode {
-    fn into_starkfelt(self) -> StarkFelt {
+    fn into_starkfelt(self) -> CoreFelt {
         match self {
-            pathfinder_common::transaction::DataAvailabilityMode::L1 => StarkFelt::ZERO,
-            pathfinder_common::transaction::DataAvailabilityMode::L2 => StarkFelt::ONE,
+            pathfinder_common::transaction::DataAvailabilityMode::L1 => CoreFelt::ZERO,
+            pathfinder_common::transaction::DataAvailabilityMode::L2 => CoreFelt::ONE,
         }
     }
 }
