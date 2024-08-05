9fa97b5c959fa84580558656d1c85991e83dedd7
sistemd <enntheprogrammer@gmail.com>
2024-08-05 11:59:17 +0200

    clippy

--- crates/pathfinder/src/sync/track.rs
+++ crates/pathfinder/src/sync/track.rs
@@ -843,7 +843,7 @@ mod tests {
             N,
             Config {
                 calculate_block_hash: Box::new(|header: &BlockHeader| {
-                    compute_final_hash(&BlockHeaderData::from_header(&header))
+                    compute_final_hash(&BlockHeaderData::from_header(header))
                 }),
                 calculate_transaction_commitment: Box::new(calculate_transaction_commitment),
                 calculate_receipt_commitment: Box::new(calculate_receipt_commitment),
