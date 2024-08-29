aeb94beb937a2e2c5771b3529f38641e4ba7bef2
Krzysztof Lis <klis33@gmail.com>
2024-08-29 15:37:23 +0200

    feat: update libp2p to 0.54.1

--- crates/p2p/src/behaviour.rs
+++ crates/p2p/src/behaviour.rs
@@ -3,6 +3,7 @@ use std::net::IpAddr;
 use std::time::{Duration, Instant};
 use std::{cmp, task};
 
+use libp2p::core::transport::PortUse;
 use libp2p::core::Endpoint;
 use libp2p::gossipsub::{self, IdentTopic};
 use libp2p::kad::store::MemoryStore;
@@ -121,6 +122,7 @@ impl NetworkBehaviour for Behaviour {
         peer: PeerId,
         addr: &Multiaddr,
         role_override: Endpoint,
+        port_use: PortUse,
     ) -> Result<THandler<Self>, ConnectionDenied> {
         // Disconnect peers without an IP address.
         Self::get_ip(addr)?;
@@ -128,8 +130,13 @@ impl NetworkBehaviour for Behaviour {
         self.check_duplicate_connection(peer)?;
         self.prevent_evicted_peer_reconnections(peer)?;
 
-        self.inner
-            .handle_established_outbound_connection(connection_id, peer, addr, role_override)
+        self.inner.handle_established_outbound_connection(
+            connection_id,
+            peer,
+            addr,
+            role_override,
+            port_use,
+        )
     }
 
     fn on_swarm_event(&mut self, event: FromSwarm<'_>) {
