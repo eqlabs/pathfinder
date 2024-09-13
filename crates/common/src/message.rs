ab5685399b3d02392fcea8019298cbd125b7c817
t00ts <abel.elbaile@gmail.com>
2024-09-13 06:58:28 +0200

    feat: store L1->L2 message logs

--- crates/common/src/message.rs
+++ crates/common/src/message.rs
@@ -1,8 +1,11 @@
 use primitive_types::H256;
 
-/// An L1 -> L2 message hash with the L1 tx hash where it was sent
+use crate::TransactionHash;
+
+/// An L1 -> L2 message log with the corresponding L1 and L2 tx hashes
 #[derive(Debug, Clone)]
 pub struct L1ToL2MessageLog {
     pub message_hash: H256,
-    pub l1_tx_hash: H256,
+    pub l1_tx_hash: Option<H256>,
+    pub l2_tx_hash: Option<TransactionHash>,
 }
