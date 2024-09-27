1c3f2069e084ae165b754bce6590f6ed1f8b12ac
sistemd <enntheprogrammer@gmail.com>
2024-09-27 13:01:25 +0200

    remove unnecessary comment

--- crates/rpc/src/v08.rs
+++ crates/rpc/src/v08.rs
@@ -5,8 +5,6 @@ use crate::method::subscribe_pending_transactions::SubscribePendingTransactions;
 #[rustfmt::skip]
 pub fn register_routes() -> RpcRouterBuilder {
     RpcRouter::builder(crate::RpcVersion::V08)
-        // TODO This is for testing - figure out if this should be removed or included when
-        // implementing RPC spec 0.8.
         .register("starknet_syncing",                      crate::method::syncing)
         .register("starknet_subscribeNewHeads",            SubscribeNewHeads)
         .register("starknet_subscribePendingTransactions", SubscribePendingTransactions)
