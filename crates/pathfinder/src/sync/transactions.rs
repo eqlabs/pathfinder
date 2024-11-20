7ca8280ca0ac698e5cb4ab2f43d9e48ec6b96622
Krzysztof Lis <klis33@gmail.com>
2024-11-20 11:53:28 +0100

    fix(transactions): CalculateHashes reports placeholder peer id

--- crates/pathfinder/src/sync/transactions.rs
+++ crates/pathfinder/src/sync/transactions.rs
@@ -85,10 +85,8 @@ impl ProcessStage for CalculateHashes {
     );
     type Output = UnverifiedTransactions;
 
-    fn map(&mut self, _: &PeerId, input: Self::Input) -> Result<Self::Output, SyncError> {
+    fn map(&mut self, peer: &PeerId, input: Self::Input) -> Result<Self::Output, SyncError> {
         use rayon::prelude::*;
-        // TODO remove the placeholder
-        let peer = &PeerId::random();
         let (transactions, block_number, version, expected_commitment) = input;
         let transactions = transactions
             .into_par_iter()
