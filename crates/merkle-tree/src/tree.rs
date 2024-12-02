c2fc124e43b48073dabbe28dfb26001310caaf99
varun-doshi <doshivarun202@gmail.com>
2024-12-02 19:46:24 +0530

    fmt

--- crates/merkle-tree/src/tree.rs
+++ crates/merkle-tree/src/tree.rs
@@ -1583,11 +1583,7 @@ mod tests {
 
     mod real_world {
         use pathfinder_common::{
-            class_commitment,
-            class_commitment_leaf_hash,
-            felt,
-            sierra_hash,
-            BlockNumber,
+            class_commitment, class_commitment_leaf_hash, felt, sierra_hash, BlockNumber,
             ClassCommitmentLeafHash,
         };
         use pathfinder_storage::RootIndexUpdate;
