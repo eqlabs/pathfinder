323eee3a58134379746ff9e5a8d1a2e9ecb83e4e
Krzysztof Lis <klis33@gmail.com>
2024-12-17 12:10:55 +0100

    fix(sync/checkpoint): rollback to anchor is not committed to the DB
    Also:
    - use util::task::spawn_blocking in place of tokio::task::spawn_blocking

--- crates/pathfinder/src/sync/checkpoint.rs
+++ crates/pathfinder/src/sync/checkpoint.rs
@@ -607,7 +607,7 @@ struct LocalState {
 impl LocalState {
     async fn from_db(storage: Storage, checkpoint: EthereumStateUpdate) -> anyhow::Result<Self> {
         // TODO: this should include header gaps.
-        spawn_blocking(move || {
+        util::task::spawn_blocking(move |_| {
             let mut db = storage
                 .connection()
                 .context("Creating database connection")?;
@@ -641,7 +641,7 @@ async fn rollback_to_anchor(
     local: BlockNumber,
     anchor: Option<BlockNumber>,
 ) -> anyhow::Result<()> {
-    spawn_blocking(move || {
+    util::task::spawn_blocking(move |_| {
         tracing::info!(%local, ?anchor, "Rolling back storage to anchor point");
 
         let last_block_to_remove = anchor.map(|n| n + 1).unwrap_or_default();
@@ -670,6 +670,8 @@ async fn rollback_to_anchor(
         transaction
             .reset()
             .context("Resetting local DB state after reorg")?;
+
+        transaction.commit().context("Committing transaction")?;
 
         Ok(())
     })
