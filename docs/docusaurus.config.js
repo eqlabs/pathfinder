81ae869631d98e80d126b2eb71561b616044ccf4
TropicolX <jacobsbusayo@gmail.com>
2025-01-09 13:32:30 +0100

    docs: update footer

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -81,43 +81,30 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Running Pathfinder',
+                to: '/getting-started/running-pathfinder',
               },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
+							{
+								label: 'Configuring Pathfinder',
+								to: '/getting-started/configuring-pathfinder',
+							},
+							{
+								label: 'Interfacing with Pathfinder',
+								to: '/interacting-with-pathfinder/json-rpc-api',
+							},
             ],
           },
           {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/eqlabs/pathfinder',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Pathfinder © ${new Date().getFullYear()}`,
       },
       prism: {
         theme: prismThemes.github,
