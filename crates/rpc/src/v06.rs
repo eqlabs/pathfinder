2cbd071c713da569dcf8f9c307521314e998bf05
Krisztian Kovacs <krisztian@equilibrium.co>
2025-01-31 13:23:58 +0100

    feat(rpc/v06): re-add transaction getter methods
    The `TXN` type is the same as we have in JSON-RPC 0.7, so the input
    and output of these methods already matches the JSON-RPC 0.6 spec.

--- crates/rpc/src/v06.rs
+++ crates/rpc/src/v06.rs
@@ -8,4 +8,6 @@ pub fn register_routes() -> RpcRouterBuilder {
         .register("starknet_getBlockTransactionCount",            crate::method::get_block_transaction_count)
         .register("starknet_getNonce",                            crate::method::get_nonce)
         .register("starknet_getStorageAt",                        crate::method::get_storage_at)
+        .register("starknet_getTransactionByHash",                crate::method::get_transaction_by_hash)
+        .register("starknet_getTransactionByBlockIdAndIndex",     crate::method::get_transaction_by_block_id_and_index)
 }
