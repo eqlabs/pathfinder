a053a0fc1999a9ef1e02cb9ba1c6c4566bcdad8b
Krisztian Kovacs <krisztian@equilibrium.co>
2025-01-31 15:21:21 +0100

    feat(rpc/v06): re-add write API
    The BROADCASTED_TRANSACTION schema is unchanged between the 0.6.0
    and 0.7.1 OpenRPC specification and the write API methods are the
    same, too.
    
    We can just re-enable `starknet_add*Transaction` methods with our
    current implementation.
    
    Closes #2553

--- crates/rpc/src/v06.rs
+++ crates/rpc/src/v06.rs
@@ -3,6 +3,9 @@ use crate::jsonrpc::{RpcRouter, RpcRouterBuilder};
 #[rustfmt::skip]
 pub fn register_routes() -> RpcRouterBuilder {
     RpcRouter::builder(crate::RpcVersion::V06)
+        .register("starknet_addDeclareTransaction",               crate::method::add_declare_transaction)
+        .register("starknet_addDeployAccountTransaction",         crate::method::add_deploy_account_transaction)
+        .register("starknet_addInvokeTransaction",                crate::method::add_invoke_transaction)
         .register("starknet_blockNumber",                         crate::method::block_number)
         .register("starknet_blockHashAndNumber",                  crate::method::block_hash_and_number)
         .register("starknet_chainId",                             crate::method::chain_id)
