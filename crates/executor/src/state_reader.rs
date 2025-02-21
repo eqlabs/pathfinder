96f4b40b66a2aa36b587da1b35a7ffa97824dd34
Krisztian Kovacs <krisztian@equilibrium.co>
2025-02-21 16:52:56 +0100

    tmp

--- crates/executor/src/state_reader.rs
+++ crates/executor/src/state_reader.rs
@@ -1,9 +1,9 @@
 use blockifier::execution::contract_class::RunnableCompiledClass;
 use blockifier::state::errors::StateError;
 use blockifier::state::state_api::StateReader;
-use cairo_vm::types::errors::program_errors::ProgramError;
 use pathfinder_common::{BlockNumber, ClassHash, StorageAddress, StorageValue};
 use pathfinder_crypto::Felt;
+use starknet_api::contract_class::SierraVersion;
 use starknet_api::StarknetApiError;
 use starknet_types_core::felt::Felt as CoreFelt;
 
@@ -88,31 +88,18 @@ impl<'tx> PathfinderStateReader<'tx> {
             Some(casm_definition) => {
                 // There's a CASM definition in storage, so this is a Sierra class. Extract
                 // class version from program.
-                let sierra_class: pathfinder_common::class_definition::Sierra<'_> =
-                    serde_json::from_slice(&class_definition)
-                        .map_err(|error| StateError::ProgramError(ProgramError::Parse(error)))?;
-                let sierra_version =
-                    starknet_api::contract_class::SierraVersion::extract_from_program(
-                        &sierra_class.sierra_program,
-                    )?;
+                let sierra_version = self.sierra_version_from_class(&class_definition)?;
+                #[cfg(feature = "cairo-native")]
+                let runnable_class = self.sierra_class_as_native(
+                    pathfinder_class_hash,
+                    sierra_version,
+                    class_definition,
+                    casm_definition,
+                )?;
+                #[cfg(not(feature = "cairo-native"))]
+                let runnable_class = self.sierra_class_as_casm(sierra_version, casm_definition)?;
 
-                let casm_definition = String::from_utf8(casm_definition).map_err(|error| {
-                    StateError::StateReadError(format!(
-                        "Class definition is not valid UTF-8: {}",
-                        error
-                    ))
-                })?;
-                let casm_class =
-                    blockifier::execution::contract_class::CompiledClassV1::try_from_json_string(
-                        &casm_definition,
-                        sierra_version,
-                    )
-                    .map_err(StateError::ProgramError)?;
-
-                Ok((
-                    definition_block_number,
-                    RunnableCompiledClass::V1(casm_class),
-                ))
+                Ok((definition_block_number, runnable_class))
             }
             None => {
                 // No CASM definition means this is a legacy Cairo 0 class.
@@ -132,6 +119,121 @@ impl<'tx> PathfinderStateReader<'tx> {
                 Ok((definition_block_number, RunnableCompiledClass::V0(class)))
             }
         }
+    }
+
+    #[cfg(feature = "cairo-native")]
+    fn sierra_class_as_native(
+        &self,
+        class_hash: ClassHash,
+        sierra_version: SierraVersion,
+        class_definition: Vec<u8>,
+        casm_definition: Vec<u8>,
+    ) -> Result<RunnableCompiledClass, StateError> {
+        use blockifier::execution::native::contract_class::NativeCompiledClassV1;
+        use cairo_native::executor::AotContractExecutor;
+        use cairo_vm::types::errors::program_errors::ProgramError;
+
+        let mut class_path = std::env::temp_dir();
+        class_path.push(format!("native_class_{}", class_hash));
+
+        let contract_executor = if class_path.is_file() {
+            AotContractExecutor::from_path(&class_path)
+                .map_err(|e| {
+                    StateError::StateReadError(format!("Error loading native class: {e}"))
+                })?
+                .expect("No locking issues with a single process")
+        } else {
+            let mut sierra_definition: serde_json::Value =
+                serde_json::from_slice(&class_definition)
+                    .map_err(|e| StateError::ProgramError(ProgramError::Parse(e)))?;
+            let sierra_abi_str = sierra_definition
+                .get("abi")
+                .ok_or_else(|| StateError::StateReadError("Sierra ABI is missing".to_owned()))?
+                .as_str()
+                .ok_or_else(|| {
+                    StateError::StateReadError("Sierra ABI is not a string".to_owned())
+                })?;
+            sierra_definition["abi"] = serde_json::from_str(sierra_abi_str)
+                .map_err(|e| StateError::ProgramError(ProgramError::Parse(e)))?;
+
+            let sierra_class: cairo_lang_starknet_classes::contract_class::ContractClass =
+                serde_json::from_value(sierra_definition)
+                    .map_err(|e| StateError::ProgramError(ProgramError::Parse(e)))?;
+
+            let sierra_program = sierra_class.extract_sierra_program().map_err(|e| {
+                StateError::StateReadError(format!(
+                    "Error parsing Sierra
+                    program: {}",
+                    e
+                ))
+            })?;
+
+            let mut class_path = std::env::temp_dir();
+            class_path.push(format!("native_class_{}", class_hash));
+            let version_id = cairo_lang_starknet_classes::compiler_version::VersionId {
+                major: sierra_version.major as usize,
+                minor: sierra_version.minor as usize,
+                patch: sierra_version.patch as usize,
+            };
+            AotContractExecutor::new_into(
+                &sierra_program,
+                &sierra_class.entry_points_by_type,
+                version_id,
+                class_path,
+                cairo_native::OptLevel::Default,
+            )
+            .map_err(|e| StateError::StateReadError(format!("Error compiling native class: {e}")))?
+            .expect("No locking issues with a single process")
+        };
+
+        let casm_definition = String::from_utf8(casm_definition).map_err(|error| {
+            StateError::StateReadError(format!("Class definition is not valid UTF-8: {}", error))
+        })?;
+
+        let casm_class =
+            blockifier::execution::contract_class::CompiledClassV1::try_from_json_string(
+                &casm_definition,
+                sierra_version,
+            )
+            .map_err(StateError::ProgramError)?;
+
+        let runnable_class = RunnableCompiledClass::V1Native(NativeCompiledClassV1::new(
+            contract_executor,
+            casm_class,
+        ));
+
+        Ok(runnable_class)
+    }
+
+    #[cfg(not(feature = "cairo-native"))]
+    fn sierra_class_as_casm(
+        &self,
+        sierra_version: SierraVersion,
+        casm_definition: Vec<u8>,
+    ) -> Result<RunnableCompiledClass, StateError> {
+        let casm_definition = String::from_utf8(casm_definition).map_err(|error| {
+            StateError::StateReadError(format!("CASM definition is not valid UTF-8: {}", error))
+        })?;
+        let casm_class =
+            blockifier::execution::contract_class::CompiledClassV1::try_from_json_string(
+                &casm_definition,
+                sierra_version,
+            )
+            .map_err(StateError::ProgramError)?;
+
+        Ok(RunnableCompiledClass::V1(casm_class))
+    }
+
+    fn sierra_version_from_class(
+        &self,
+        class_definition: &[u8],
+    ) -> Result<SierraVersion, StateError> {
+        use cairo_vm::types::errors::program_errors::ProgramError;
+
+        let sierra_class: pathfinder_common::class_definition::Sierra<'_> =
+            serde_json::from_slice(class_definition)
+                .map_err(|error| StateError::ProgramError(ProgramError::Parse(error)))?;
+        SierraVersion::extract_from_program(&sierra_class.sierra_program).map_err(Into::into)
     }
 }
 
