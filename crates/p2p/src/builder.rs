fbd66d5e8216ca24dfa788af53ca32c6830326a0
Krzysztof Lis <klis33@gmail.com>
2025-03-12 12:02:09 +0100

    fix(swarm): per event connection buffer size is too big

--- crates/p2p/src/builder.rs
+++ crates/p2p/src/builder.rs
@@ -46,8 +46,6 @@ impl Builder {
         let (command_sender, command_receiver) = mpsc::channel(1);
         let client = Client::new(command_sender, local_peer_id);
 
-        let max_concurrent_streams = cfg.max_concurrent_streams;
-
         let (behaviour, relay_transport) = behaviour_builder
             .unwrap_or_else(|| Behaviour::builder(keypair.clone(), chain_id, cfg))
             .build();
@@ -56,25 +54,7 @@ impl Builder {
             transport::create(&keypair, relay_transport),
             behaviour,
             local_peer_id,
-            swarm::Config::with_tokio_executor()
-                // The swarm will stall if the length of the event buffer is too small.
-                // https://github.com/libp2p/rust-libp2p/blob/a966f626af5f3243a67207856c6a79df846d316a/swarm/src/connection/pool.rs#L1014
-                //
-                // See PR https://github.com/eqlabs/pathfinder/pull/2594 for more details.
-                //
-                // In the worst case, for each stream, we need to accommodate for at least:
-                // - 1x `InboundRequest` event
-                // - 1x `OutboundResponseStreamClosed` event, in case the older streams are closed
-                //   and immediately replaced by new ones from the very same client
-                // - a number of other events, that could happen in the meantime from other
-                //   behaviors: identify, kad, ping, gossipsub, etc.
-                //
-                // Stress tests show that 3x should be sufficient, so we're using 4x just to be
-                // safe.
-                //
-                // We also add a base value of 7, which is the default size of this buffer, in case
-                // `max_concurrent_streams` is 1.
-                .with_per_connection_event_buffer_size(7 + max_concurrent_streams * 4),
+            swarm::Config::with_tokio_executor(),
         );
 
         let (event_sender, event_receiver) = mpsc::channel(1);
