5a2c288a31eb84215f86270c96591af86867d5c2
Krisztian Kovacs <krisztian@equilibrium.co>
2025-03-28 10:56:25 +0100

    feat(rpc): create a span for each subscription

--- crates/rpc/src/jsonrpc/router/subscription.rs
+++ crates/rpc/src/jsonrpc/router/subscription.rs
@@ -236,7 +236,7 @@ where
                         tx.send_err(e).await.ok();
                     }
                 }
-            });
+            }.in_current_span());
             let first_msg = match rx1.recv().await {
                 Some(msg) => msg,
                 None => {
@@ -305,7 +305,7 @@ where
                 }
                 last_block = msg.block_number;
             }
-        }))
+        }.instrument(tracing::debug_span!("subscription", subscription_id=%subscription_id.0))))
     }
 }
 
@@ -550,7 +550,7 @@ pub fn handle_json_rpc_socket(
                 }
             }
         }
-    });
+    }.in_current_span());
 }
 
 /// Handle a single request. Returns `Result` for convenience, so that the `?`
