aa8cb32c44a597e7e7ff2ec7e6824fde08986261
Krzysztof Lis <klis33@gmail.com>
2025-03-31 23:42:33 +0200

    chore(p2p-v2): remove example

--- crates/p2p-v2/examples/basic.rs
+++
@@ -1,8 +0,0 @@
-// use p2p_v2::sync::Behaviour;
-// use p2p_v2::MainLoop;
-
-fn main() {
-    // FIXME
-    // let sync = Behaviour::new();
-    // let p2p = MainLoop::new(sync, command_tx, event_rx);
-}
