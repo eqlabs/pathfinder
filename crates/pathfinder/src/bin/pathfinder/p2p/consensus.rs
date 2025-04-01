899ee59da186a5bf8331e508853c488e195feb33
Krzysztof Lis <klis33@gmail.com>
2025-04-01 13:04:28 +0200

    chore: remove commented code

--- crates/pathfinder/src/bin/pathfinder/p2p/consensus.rs
+++ crates/pathfinder/src/bin/pathfinder/p2p/consensus.rs
@@ -26,36 +26,6 @@ async fn start_inner(
     _chain_id: ChainId,
     _config: P2PConsensusConfig,
 ) -> anyhow::Result<(JoinHandle<anyhow::Result<()>>, Option<Client>)> {
-    /* TODO
-    let context = P2PContext {
-        cfg: p2p::Config {
-            direct_connection_timeout: config.core.direct_connection_timeout,
-            relay_connection_timeout: Duration::from_secs(10),
-            max_inbound_direct_peers: config.core.max_inbound_direct_connections,
-            max_inbound_relayed_peers: config.core.max_inbound_relayed_connections,
-            max_outbound_peers: config.core.max_outbound_connections,
-            ip_whitelist: config.core.ip_whitelist,
-            bootstrap_period: Some(Duration::from_secs(2 * 60)),
-            eviction_timeout: config.core.eviction_timeout,
-            inbound_connections_rate_limit: p2p::RateLimit {
-                max: 10,
-                interval: Duration::from_secs(1),
-            },
-            kad_name: config.core.kad_name,
-            stream_timeout: config.stream_timeout,
-            response_timeout: config.response_timeout,
-            max_concurrent_streams: config.max_concurrent_streams,
-        },
-        chain_id,
-        storage,
-        keypair: identity::load_or_generate(config.core.identity_config_file)?,
-        listen_on: config.core.listen_on,
-        bootstrap_addresses: config.core.bootstrap_addresses,
-        predefined_peers: config.core.predefined_peers,
-    };
-
-    let (p2p_client, p2p_handle) = start(context).await?;
-    */
     let p2p_client = ();
     let p2p_handle = tokio::task::spawn(futures::future::pending());
 
