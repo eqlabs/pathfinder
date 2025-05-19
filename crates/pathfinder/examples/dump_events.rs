57ad70612d0959f744738fce09472a27448d824b
Vaclav Barta <vbarta@mangrove.cz>
2025-05-19 11:09:51 +0200

    feat: optional filtering by keys

--- crates/pathfinder/examples/dump_events.rs
+++ crates/pathfinder/examples/dump_events.rs
@@ -5,8 +5,8 @@ use std::path::PathBuf;
 
 use anyhow::anyhow;
 use clap::Parser;
-use pathfinder_common::{BlockNumber, ContractAddress};
-use pathfinder_crypto::Felt;
+use pathfinder_common::{BlockNumber, ContractAddress, EventKey};
+use pathfinder_crypto::{Felt, HexParseError};
 use serde_json::json;
 
 #[derive(Parser)]
@@ -44,6 +44,13 @@ pub struct Cli {
     pub address: Option<String>,
     #[arg(
         long,
+        short = 'k',
+        value_name = "list",
+        long_help = "Filter output by these keys"
+    )]
+    pub keys: Option<String>,
+    #[arg(
+        long,
         value_name = "output",
         long_help = "Path to output directory",
         default_value = "ground"
@@ -58,16 +65,29 @@ fn make_ground_path(cli: &Cli) -> anyhow::Result<PathBuf> {
     } else {
         format!("{}+{}", cli.from_block, cli.to_block - cli.from_block)
     };
-    if let Some(addr) = &cli.address {
-        let filter_number = 0;
-        let filter_json = json!({
-            "address": addr
-        });
-        let filter_name = format!("{}f{}.json", basename, filter_number);
-        let filter_path = dir.join(filter_name);
-        fs::write(filter_path, filter_json.to_string())?;
 
-        basename = format!("{}w{}", basename, filter_number);
+    let filter_number = 0;
+    let mut filter_json = json!({});
+    if let Some(addr) = &cli.address {
+        filter_json["address"] = json!(addr);
+    }
+
+    if let Some(raw_keys) = &cli.keys {
+        let keys: Vec<_> = raw_keys
+            .split(',')
+            .map(|s| if s.is_empty() { vec![] } else { vec![s] })
+            .collect();
+        filter_json["keys"] = json!(keys);
+    }
+
+    if let serde_json::Value::Object(ref filter_map) = filter_json {
+        if !filter_map.is_empty() {
+            let filter_name = format!("{}f{}.json", basename, filter_number);
+            let filter_path = dir.join(filter_name);
+            fs::write(filter_path, filter_json.to_string())?;
+
+            basename = format!("{}w{}", basename, filter_number);
+        }
     }
 
     let name = format!("{}.jsonl", basename);
@@ -86,24 +106,65 @@ fn main() -> anyhow::Result<()> {
     fs::create_dir_all(&cli.output_dir)?;
     let output_path = make_ground_path(&cli)?;
     let mut output_file = fs::File::create(output_path)?;
+
     let address = if let Some(addr) = cli.address {
         let felt = Felt::from_hex_str(&addr)?;
         Some(ContractAddress::new_or_panic(felt))
     } else {
         None
     };
+
+    let keys = if let Some(mut raw_keys) = cli.keys {
+        while let Some(tail) = raw_keys.chars().last() {
+            if tail == ',' {
+                raw_keys.pop();
+            } else {
+                break;
+            }
+        }
+        let cooked_keys = raw_keys
+            .split(',')
+            .map(|s| {
+                if s.is_empty() {
+                    Ok(None)
+                } else {
+                    let f = Felt::from_hex_str(s)?;
+                    Ok(Some(EventKey(f)))
+                }
+            })
+            .collect::<Result<Vec<Option<EventKey>>, HexParseError>>()?;
+        Some(cooked_keys)
+    } else {
+        None
+    };
+
     for n in cli.from_block..=cli.to_block {
         let bn = BlockNumber::new(n).ok_or_else(|| anyhow!("invalid block number {}", n))?;
         if let Some(pairs) = db_tx.events_for_block(bn.into())? {
             for pair in pairs {
                 let tx_hash = pair.0;
                 for event in &pair.1 {
-                    let accept = if let Some(addr) = address {
+                    let accept_address = if let Some(addr) = address {
                         event.from_address == addr
                     } else {
                         true
                     };
-                    if accept {
+                    let accept_keys = if let Some(ref keys) = keys {
+                        if keys.len() > event.keys.len() {
+                            false
+                        } else {
+                            keys.iter().zip(event.keys.iter()).all(|(k, ek)| {
+                                if let Some(k) = k {
+                                    *k == *ek
+                                } else {
+                                    true
+                                }
+                            })
+                        }
+                    } else {
+                        true
+                    };
+                    if accept_address && accept_keys {
                         let v = json!({
                             "block_number": n,
                             "data": event.data,
