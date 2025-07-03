ce13154ce04b930ddc5f2451d06e9d1eb9c23a4e
Vaclav Barta <vbarta@mangrove.cz>
2025-07-03 12:22:39 +0200

    fix: actually change default value of L2 gas to 1

--- crates/storage/src/schema/revision_0072.rs
+++ crates/storage/src/schema/revision_0072.rs
@@ -1,17 +1,18 @@
 use anyhow::Context;
-use rusqlite::params;
 
 pub(crate) fn migrate(tx: &rusqlite::Transaction<'_>) -> anyhow::Result<()> {
     tracing::info!("Changing l2_gas_price default value");
 
     tx.execute(
-        "UPDATE block_headers SET eth_l2_gas_price = ? WHERE eth_l2_gas_price = ?",
-        params![1, 0],
+        "UPDATE block_headers SET eth_l2_gas_price = X'00000000000000000000000000000001' WHERE \
+         eth_l2_gas_price = X'00000000000000000000000000000000'",
+        [],
     )
     .context("Updating block_headers.eth_l2_gas_price")?;
     tx.execute(
-        "UPDATE block_headers SET strk_l2_gas_price = ? WHERE strk_l2_gas_price = ?",
-        params![1, 0],
+        "UPDATE block_headers SET strk_l2_gas_price = X'00000000000000000000000000000001' WHERE \
+         strk_l2_gas_price = X'00000000000000000000000000000000'",
+        [],
     )
     .context("Updating block_headers.strk_l2_gas_price")?;
 
