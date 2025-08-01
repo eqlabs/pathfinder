4503e5d06355e585828a70f68eba521e84d83bf3
Krisztian Kovacs <krisztian@equilibrium.co>
2025-08-01 23:16:48 +0200

    chore(cargo): upgrade futures-bounded to 0.3.0

--- crates/p2p_stream/src/handler.rs
+++ crates/p2p_stream/src/handler.rs
@@ -130,7 +130,7 @@ where
             pending_events: VecDeque::new(),
             inbound_request_id,
             worker_streams: futures_bounded::FuturesMap::new(
-                substream_timeout,
+                move || futures_bounded::Delay::futures_timer(substream_timeout),
                 max_concurrent_streams,
             ),
             response_timeout,
