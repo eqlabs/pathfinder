580496aa76e505501f05fb47661930531f4e1c71
Krzysztof Lis <klis33@gmail.com>
2025-10-10 14:09:53 +0200

    refactor(consensus_smoke): extract utilities for integration testing

--- crates/pathfinder/tests/common/mod.rs
+++ crates/pathfinder/tests/common/mod.rs
@@ -2,3 +2,4 @@
 
 pub mod pathfinder_instance;
 pub mod rpc_client;
+pub mod utils;
--- crates/pathfinder/tests/consensus_smoke.rs
+++ crates/pathfinder/tests/consensus_smoke.rs
@@ -13,128 +13,77 @@ mod common;
 
 #[cfg(feature = "p2p")]
 mod test {
-    use std::time::{Duration, Instant};
+    mod consensus_3_nodes {
+        use std::time::Duration;
+        use std::vec;
 
-    use anyhow::Context;
-    use tokio::time::sleep;
+        use anyhow::Context;
+        use tokio::task::JoinError;
+        use tokio::time::sleep;
 
-    use super::common::pathfinder_instance::{
-        fixture_dir,
-        pathfinder_bin,
-        Config,
-        PathfinderInstance,
-    };
-    use super::common::rpc_client::wait_for_height;
-    use crate::common::pathfinder_instance::respawn_on_fail;
+        use crate::common::pathfinder_instance::{respawn_on_fail, PathfinderInstance};
+        use crate::common::rpc_client::wait_for_height;
+        use crate::common::utils;
 
-    // If the env variable `PATHFINDER_CONSENSUS_TEST_DUMP_CHILD_LOGS_ON_FAIL` is
-    // set, the stdout and stderr logs of each Pathfinder instance will be
-    // dumped automatically to the parent process descriptors if the test fails.
-    // Otherwise you need to inspect the temporary directory that is created to
-    // hold the test artifacts.
-    #[tokio::test]
-    // TODO rename consensus_3_nodes_happy_path
-    async fn consensus_3_node_smoke_test() -> anyhow::Result<()> {
-        PathfinderInstance::enable_log_dump(
-            std::env::var_os("PATHFINDER_CONSENSUS_TEST_DUMP_CHILD_LOGS_ON_FAIL").is_some(),
-        );
+        // If the env variable `PATHFINDER_CONSENSUS_TEST_DUMP_CHILD_LOGS_ON_FAIL` is
+        // set, the stdout and stderr logs of each Pathfinder instance will be
+        // dumped automatically to the parent process descriptors if the test fails.
+        // Otherwise you need to inspect the temporary directory that is created to
+        // hold the test artifacts.
+        #[tokio::test]
+        async fn happy_path() -> anyhow::Result<()> {
+            const NUM_NODES: usize = 3;
+            const HEIGHT: u64 = 20;
+            const READY_TIMEOUT: Duration = Duration::from_secs(20);
+            const TEST_TIMEOUT: Duration = Duration::from_secs(120);
+            const POLL_READY: Duration = Duration::from_millis(500);
+            const POLL_HEIGHT: Duration = Duration::from_secs(1);
 
-        const NUM_NODES: usize = 3;
-        const MIN_REQUIRED_DECIDED_HEIGHT: u64 = 20;
-        const READY_TIMEOUT: Duration = Duration::from_secs(20);
-        const TEST_TIMEOUT: Duration = Duration::from_secs(120);
-        const READY_POLL_INTERVAL: Duration = Duration::from_millis(500);
-        const HEIGHT_POLL_INTERVAL: Duration = Duration::from_secs(1);
+            let (configs, stopwatch) = utils::setup(NUM_NODES)?;
+            let mut configs = configs.into_iter();
 
-        let stopwatch = Instant::now();
+            let alice = PathfinderInstance::spawn(configs.next().unwrap())?;
+            alice.wait_for_ready(POLL_READY, READY_TIMEOUT).await?;
 
-        let pathfinder_bin = pathfinder_bin();
-        let fixture_dir = fixture_dir();
-        let test_dir = tempfile::Builder::new()
-            .disable_cleanup(true)
-            .tempdir()
-            .context("Creating temporary directory for test artifacts")?;
-        println!(
-            "Test artifacts will be stored in {}",
-            test_dir.path().display()
-        );
+            let bob_cfg = configs.next().unwrap();
 
-        assert!(pathfinder_bin.exists(), "Pathfinder binary not found");
-        assert!(fixture_dir.exists(), "Fixture directory not found");
+            let bob = PathfinderInstance::spawn(bob_cfg.clone())?;
+            let charlie = PathfinderInstance::spawn(configs.next().unwrap())?;
 
-        let mut configs =
-            Config::for_set(NUM_NODES, &pathfinder_bin, &fixture_dir, test_dir.path()).into_iter();
+            let (bob_rdy, charlie_rdy) = tokio::join!(
+                bob.wait_for_ready(POLL_READY, READY_TIMEOUT),
+                charlie.wait_for_ready(POLL_READY, READY_TIMEOUT)
+            );
+            bob_rdy?;
+            charlie_rdy?;
 
-        let alice = PathfinderInstance::spawn(configs.next().unwrap())?;
-        alice
-            .wait_for_ready(READY_POLL_INTERVAL, READY_TIMEOUT)
-            .await?;
+            utils::log_elapsed(stopwatch);
 
-        let bob_cfg = configs.next().unwrap();
-        let bob_rpc_port = bob_cfg.rpc_port;
+            let alice_client = wait_for_height(&alice, HEIGHT, POLL_HEIGHT);
+            let bob_client = wait_for_height(&bob, HEIGHT, POLL_HEIGHT);
+            let charlie_client = wait_for_height(&charlie, HEIGHT, POLL_HEIGHT);
 
-        let mut bob = PathfinderInstance::spawn(bob_cfg.clone())?;
-        let charlie = PathfinderInstance::spawn(configs.next().unwrap())?;
+            let _guard = respawn_on_fail(bob, bob_cfg, POLL_READY, READY_TIMEOUT, TEST_TIMEOUT);
 
-        let (bob_rdy, charlie_rdy) = tokio::join!(
-            bob.wait_for_ready(READY_POLL_INTERVAL, READY_TIMEOUT),
-            charlie.wait_for_ready(READY_POLL_INTERVAL, READY_TIMEOUT)
-        );
-        bob_rdy?;
-        charlie_rdy?;
+            tokio::select! {
+                _ = sleep(TEST_TIMEOUT) => {
+                    eprintln!("Test timed out after {TEST_TIMEOUT:?}");
+                    Err(anyhow::anyhow!("Test timed out after {TEST_TIMEOUT:?}"))
+                }
 
-        let spawn_rpc_client = |instance: &PathfinderInstance| {
-            tokio::spawn(wait_for_height(
-                instance.name(),
-                instance.rpc_port(),
-                MIN_REQUIRED_DECIDED_HEIGHT,
-                HEIGHT_POLL_INTERVAL,
-            ))
-        };
+                test_result = futures::future::join_all(vec![alice_client, bob_client, charlie_client]) => {
+                    test_result.into_iter().collect::<Result<Vec<_>, JoinError>>().context("Joining all RPC client tasks")?;
 
-        println!(
-            "All RPC clients and nodes spawned and ready after {} s",
-            stopwatch.elapsed().as_secs()
-        );
+                    // Don't dump logs if the test succeeded.
+                    PathfinderInstance::enable_log_dump(false);
+                    Ok(())
+                }
 
-        let alice_client = spawn_rpc_client(&alice);
-        let bob_client = spawn_rpc_client(&bob);
-        let charlie_client = spawn_rpc_client(&charlie);
-
-        let _guard = respawn_on_fail(
-            bob,
-            bob_cfg,
-            READY_POLL_INTERVAL,
-            READY_TIMEOUT,
-            TEST_TIMEOUT,
-        );
-
-        let test_result = tokio::select! {
-            _ = sleep(TEST_TIMEOUT) => {
-                eprintln!("Test timed out after {TEST_TIMEOUT:?}");
-                Err(anyhow::anyhow!("Test timed out after {TEST_TIMEOUT:?}"))
+                _ = tokio::signal::ctrl_c() => {
+                    eprintln!("Received Ctrl-C, terminating test early");
+                    Err(anyhow::anyhow!("Test interrupted by user"))
+                }
             }
-
-            test_result = async {
-                tokio::join!(alice_client, bob_client, charlie_client)
-            } => {
-                let (a, b, c) = test_result;
-                a.context("Joining Alice's RPC client task")?;
-                b.context("Joining Bob's RPC client task")?;
-                c.context("Joining Charlie's RPC client task")?;
-                // Don't dump logs if the test succeeded.
-                PathfinderInstance::enable_log_dump(false);
-                Ok(())
-            }
-
-            _ = tokio::signal::ctrl_c() => {
-                eprintln!("Received Ctrl-C, terminating test early");
-                Err(anyhow::anyhow!("Test interrupted by user"))
-            }
-        };
-
-        test_result
+        }
     }
-
-    async fn consensus_3_nodes_bob_fails_and_recovers() {}
 }
