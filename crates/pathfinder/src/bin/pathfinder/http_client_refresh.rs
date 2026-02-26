afcaa5495f12a0b9061038416384e426a360bcf0
Krisztian Kovacs <krisztian@equilibrium.co>
2026-02-26 17:10:38 +0100

    fix(pathfinder): refresh the HTTP client in a blocking context

--- crates/pathfinder/src/bin/pathfinder/http_client_refresh.rs
+++ crates/pathfinder/src/bin/pathfinder/http_client_refresh.rs
@@ -5,9 +5,14 @@ pub async fn refresh_http_client_periodically(client: Client) -> anyhow::Result<
         // Refresh the HTTP client every minute to ensure that we have a fresh
         // connection pool and updated DNS information.
         tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-        // Ignore the result of refresh since it can fail due to transient network
-        // issues, and we don't want to crash the entire application because of that.
-        // We'll just try again in the next cycle.
-        let _ = client.refresh();
+
+        // Address resolution using `std::net` is blocking, so we need to run it in a
+        // blocking context to avoid blocking the async runtime.
+        tokio::task::block_in_place(|| {
+            // Ignore the result of refresh since it can fail due to transient network
+            // issues, and we don't want to crash the entire application because of that.
+            // We'll just try again in the next cycle.
+            let _ = client.refresh();
+        });
     }
 }
